23066ced84a648982a6d03a58fa5b89fe271bb47
mirage1129 <meraj.a.qazi@gmail.com>
2018-08-01 21:11:06 +0800

    Finished till winning state

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 class Board extends React.Component {
     constructor(){
       super()
+      this.clickHandler = this.clickHandler.bind(this);
     }
 
     state = {
       board: [
-        ['','',''],
-        ['','',''],
-        ['','','']
+        ['O','O','O'],
+        ['O','O','O'],
+        ['O','O','O']
       ]
     }
 
+    
+    clickHandler(ev,colIndex,rowIndex){
+      if (this.state.board[rowIndex][colIndex] === "X") {
+        this.state.board[rowIndex][colIndex] = "O"
+        let currentBoard = this.state.board
+        this.setState( { board: currentBoard } );
+      } else {
+        this.state.board[rowIndex][colIndex] = "X"
+        let currentBoard = this.state.board
+        this.setState( { board: currentBoard } );
+      }
+
+      var currentBoard = this.state.board
+      if (currentBoard[0][0] === "X" && currentBoard[0][1] === "X" && currentBoard[0][2] === "X"
+          || currentBoard[1][0] === "X" && currentBoard[1][1] === "X" && currentBoard[1][2] === "X"
+          || currentBoard[2][0] === "X" && currentBoard[2][1] === "X" && currentBoard[2][2] === "X"
+          || currentBoard[0][0] === "X" && currentBoard[1][0] === "X" && currentBoard[2][0] === "X"
+          || currentBoard[0][1] === "X" && currentBoard[1][1] === "X" && currentBoard[2][1] === "X"
+          || currentBoard[0][2] === "X" && currentBoard[1][2] === "X" && currentBoard[2][2] === "X"
+          || currentBoard[0][0] === "X" && currentBoard[1][1] === "X" && currentBoard[2][2] === "X"
+          || currentBoard[0][2] === "X" && currentBoard[1][1] === "X" && currentBoard[2][0] === "X") {
+            console.log(currentBoard)
+            alert("you won!")
+      }
+    }
+
     render() {
-        console.log("board", this.state.board);
+        // console.log("board", this.state.board);
         const board = this.state.board.map( (row,rowIndex) => {
           const rows = row.map( (col,colIndex) => {
-            return (
-                    <span>{col} : {rowIndex}</span>
+
+            
+            return (            
+              <span onClick={((ev) => this.clickHandler(ev, colIndex, rowIndex))}>{col}</span>
             );
 
           });
@@ -41,3 +70,4 @@ ReactDOM.render(
     <Board/>,
     document.getElementById('root')
 );
+
