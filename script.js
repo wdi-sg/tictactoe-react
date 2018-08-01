1a5a626e07ab39fa58ee31eb1cfc1dd4eb820d2d
jodich <jodi_choo@hotmail.com>
2018-08-01 21:40:40 +0800

    further 1

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 class Board extends React.Component {
     constructor(){
       super()
+      this.clickHandler = this.clickHandler.bind(this);
+      this.counter = 0;
     }
 
     state = {
@@ -11,30 +13,92 @@ class Board extends React.Component {
       ]
     }
 
+    checkWins(rowIndex, colIndex) {
+      console.log(this.state.board[rowIndex][colIndex])
+    }
+
+    clickHandler(rowIndex, colIndex) {
+      // console.log('row is', rowIndex);
+      // console.log('col is', colIndex);
+
+      if (this.state.board[rowIndex][colIndex] === 'X' || this.state.board[rowIndex][colIndex] === 'O') {
+        console.log('cannot');
+        return
+      }
+
+      this.counter ++ 
+
+      if (this.counter % 2 == 0 ) {
+        this.state.board[rowIndex][colIndex] = 'X';
+      } else {
+        this.state.board[rowIndex][colIndex] = 'O';
+      }
+      
+      let newBoard = this.state.board;
+      this.setState( {board: newBoard} );
+
+      this.checkWins(rowIndex, colIndex);
+
+    }
+
     render() {
-        console.log("board", this.state.board);
-        const board = this.state.board.map( (row,rowIndex) => {
-          const rows = row.map( (col,colIndex) => {
-            return (
-                    <span>{col} : {rowIndex}</span>
-            );
+        // console.log("board", this.state.board);
+        
+        // const board = this.state.board.map( (row,rowIndex) => {
+        //   const rows = row.map( (col,colIndex) => {
 
-          });
-          return (
-            <div className="row">
-              {rows}
-            </div>
+        //     let newClickHandle = () => { 
+        //       this.clickHandler(rowIndex, colIndex) 
+        //     }
+           
+        //     return (
+        //             <div className="span" onClick={newClickHandle} >{this.state.board[rowIndex][colIndex]}</div>
+        //     );
 
-          );
+        //   });
+        //   return (
+        //     <div className="row">
+        //       {rows}
+        //     </div>
 
-        });
+        //   );
+
+        // });
 
         return (
           <div className="item">
-            {board}
+            <Game board={this.state.board} clickHandler={this.clickHandler}/>
           </div>
         );
     }
+}
+
+class Game extends React.Component {
+
+  render() {
+    // console.log("board", this.state.board);
+    return (
+    
+      this.props.board.map( (row,rowIndex) => {
+        const rows = row.map( (col,colIndex) => {
+
+          let newClickHandle = () => { 
+            this.props.clickHandler(rowIndex, colIndex) 
+          }
+        
+          return (
+                  <div className="span" onClick={newClickHandle} >{this.props.board[rowIndex][colIndex]}</div>
+          );
+
+        });
+        return (
+          <div className="row">
+            {rows}
+          </div>
+        );
+      })
+    );
+  }
 }
 
 ReactDOM.render(
