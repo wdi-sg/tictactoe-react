7e7cbcada2a1bb841d5adb9156418edcaac40c21
Lee Wen Yao <peterleewenyao@gmail.com>
2018-08-02 18:29:02 +0800

    homewoek

--- script.jsx
+++ script.jsx
@@ -4,22 +4,40 @@ class Board extends React.Component {
     }
 
     state = {
-      board: [ ['','',''], ['','',''], ['','','']
-      ]
-
-
+      board: [ 
+        ['','',''], 
+        ['','',''], 
+        ['','','']
+      ],
+      x: "true",
+      o: "false"
     }
 
-    // clickHandler(){
-      
-    // }
+    clickHandler(event, colIndex, rowIndex){
+      let gameBoard = this.state.board;
+
+      if (this.state.x === "true"){
+        gameBoard[rowIndex][colIndex] = "x"
+        this.setState({board:gameBoard})
+        this.setState({x:"false"})
+        this.setState({o:"true"})      
+      }
+
+      if (this.state.x === "true"){
+        gameBoard[rowIndex][colIndex] = "o"
+        this.setState({board:gameBoard})
+        this.setState({x:"true"})
+        this.setState({o:"false"})
+      }
+    }
+
 
     render() {
         console.log("board", this.state.board);
         const board = this.state.board.map( (row,rowIndex) => {
           const rows = row.map( (col,colIndex) => {
             return (
-              <span>{col} : {rowIndex}</span>
+              <span onClick={this.clickHandler}>{col} : {rowIndex}</span>
             );
           });
           return (
