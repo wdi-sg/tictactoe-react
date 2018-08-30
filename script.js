bf2e3cb53230e26eabb416aa66c97cde6c351a2a
acechua <acecyq@gmail.com>
2018-08-30 21:51:39 +0800

    add names section. refactor code.

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+class Names extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  state={
+    play0 : "test",
+    play1 : ""
+  }
+
+  handleChange(player, event) {
+    var name = event.target.value;
+    if (player === "play0") {
+      this.setState({
+        play0 : name
+      });  
+    } else {
+      this.setState({
+        play1 : name
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1><span>{this.state.play0}</span> VS <span>{this.state.play1}</span></h1>
+        <label htmlFor="play0">Player 1</label>
+        <input id="play0" type="text" onChange={(event) => this.handleChange("play0", event)}></input>
+        <label htmlFor="play1">Player 2</label>
+        <input id="play1" type="text" onChange={(event) => this.handleChange("play1", event)}></input>
+      </div>
+    );
+  }
+}
+
 class Board extends React.Component {
-    
-	//testing
   constructor() {
     super();
     this.handleClick = this.handleClick.bind(this);
@@ -30,12 +65,14 @@ class Board extends React.Component {
 
   render() {
     const board = this.state.board.map( (row,rowIndex) => {
-      const rows = row.map( (col,colIndex) => { return (
-              <span id="{colIndex},{rowIndex}" onClick={((ev) => this.handleClick(rowIndex, colIndex))}>
-                | {col}
-              </span>
-            );
-          });
+      const rows = row.map( (col,colIndex) => {
+        var name = colIndex.toString() + "," + rowIndex.toString();
+        return (
+          <span id={name} key={name} onClick={() => this.handleClick(rowIndex, colIndex)}>
+            | {col}
+          </span>
+        );
+      });
       return (
         <div className="row">
           {rows}
@@ -45,7 +82,7 @@ class Board extends React.Component {
     });
 
     return (
-      <div className="item">
+      <div key="board" className="item">
         {board}
       </div>
     );
@@ -60,6 +97,7 @@ class Game extends React.Component {
   render() {
     return(
       <div>
+        <Names/>
         <Board/>
       </div>
     )
