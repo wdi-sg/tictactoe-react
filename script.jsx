158253ca7e9f2f700a9afc1d2d8ef876fd070f26
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-11-08 15:06:20 +0800

    Init prettier/eslint format

--- script.jsx
+++ script.jsx
@@ -1,65 +1,49 @@
 class Board extends React.Component {
-    constructor(){
+  constructor() {
+    super();
 
-      super()
+    this.state = {
+      board: [['i', 'i', 'i'], ['i', 'i', 'i'], ['i', 'i', 'i']],
+    };
+  }
 
-      this.state = {
-        board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
-        ]
-      }
+  squareClick(something) {
+    console.log(something);
+  }
 
-    }
+  render() {
+    console.log('board', this.state.board);
 
-    squareClick(something){
-        console.log( something );
-    }
+    const board = this.state.board.map((row, rowIndex) => {
+      // make a single row
+      const rows = row.map((col, colIndex) => (
+        <p
+          className="boo"
+          key={colIndex}
+          onClick={() => {
+            this.squareClick(colIndex);
+          }}
+        >
+          {col}
+          {' '}
+:
+          {colIndex}
+          {' '}
+:
+          {rowIndex}
+        </p>
+      ));
 
-    render() {
-        console.log("board", this.state.board);
+      // return the complete row
+      return (
+        <div key={rowIndex} className="row">
+          {rows}
+        </div>
+      );
+    });
 
-        const board = this.state.board.map( (row,rowIndex) => {
-
-          // make a single row
-          const rows = row.map( (col,colIndex) => {
-
-            // make each column
-            return (
-                    <p
-                        className="boo"
-                        key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex);
-                        }}
-
-                    >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
-            );
-
-          });
-
-          // return the complete row
-          return (
-            <div key={rowIndex} className="row">
-              {rows}
-            </div>
-
-          );
-
-        });
-
-        return (
-          <div className="item">
-            {board}
-          </div>
-        );
-    }
+    return <div className="item">{board}</div>;
+  }
 }
 
-ReactDOM.render(
-    <Board/>,
-    document.getElementById('root')
-);
+ReactDOM.render(<Board />, document.getElementById('root'));
