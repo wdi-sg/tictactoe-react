b8b4da58a408ac4dcfff560d12cb24ad3e5723ae
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-11-08 16:11:00 +0800

    Complete tictacoe board

--- script.jsx
+++ script.jsx
@@ -1,49 +1,46 @@
 class Board extends React.Component {
   constructor() {
     super();
-
     this.state = {
-      board: [['i', 'i', 'i'], ['i', 'i', 'i'], ['i', 'i', 'i']],
+      board: [['*', '*', '*'], ['*', '*', '*'], ['*', '*', '*']],
+      turn: 0,
     };
   }
 
-  squareClick(something) {
-    console.log(something);
-  }
+  squareClick = (row, col) => {
+    console.log('row:', row, 'col:', col);
+    let updateBoard = this.state.board.slice(0);
+    this.state.turn % 2 == 0 ? (updateBoard[col][row] = 'X') : (updateBoard[col][row] = '0');
+    this.setState({ board: updateBoard });
+    this.state.turn++;
+  };
 
   render() {
-    console.log('board', this.state.board);
-
     const board = this.state.board.map((row, rowIndex) => {
-      // make a single row
-      const rows = row.map((col, colIndex) => (
-        <p
-          className="boo"
+      const cols = row.map((col, colIndex) => (
+        <div
           key={colIndex}
-          onClick={() => {
-            this.squareClick(colIndex);
-          }}
+          className="square d-flex flex-column justify-content-center align-items-center"
+          onClick={() => this.squareClick(colIndex, rowIndex)}
         >
-          {col}
-          {' '}
-:
-          {colIndex}
-          {' '}
-:
-          {rowIndex}
-        </p>
+          <span className="display-3 font-weight-bold">{col}</span>
+        </div>
       ));
-
-      // return the complete row
       return (
-        <div key={rowIndex} className="row">
-          {rows}
+        <div key={rowIndex} className="column">
+          {cols}
         </div>
       );
     });
-
-    return <div className="item">{board}</div>;
+    return <div className="grid">{board}</div>;
   }
 }
 
-ReactDOM.render(<Board />, document.getElementById('root'));
+ReactDOM.render(
+  <div className="container-fluid h-100">
+    <div className="d-flex h-100 flex-column align-items-center justify-content-center">
+      <Board />
+    </div>
+  </div>,
+  document.getElementById('root'),
+);
