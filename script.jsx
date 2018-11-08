fa3d3cd9e38be86cd609f8873b726c218c04aacc
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-11-08 22:53:49 +0800

    Tweaks and fixes

--- script.jsx
+++ script.jsx
@@ -1,18 +1,18 @@
 class Board extends React.Component {
   render() {
     const board = this.props.board.map((row, rowIndex) => {
-      const cols = row.map((col, colIndex) => (
+      const rows = row.map((col, colIndex) => (
         <div
           key={colIndex}
           className="square d-flex flex-column justify-content-center align-items-center"
-          onClick={() => this.props.squareClick(colIndex, rowIndex)}
+          onClick={() => this.props.squareClick(rowIndex, colIndex)}
         >
           <span className="display-3 font-weight-bold">{col}</span>
         </div>
       ));
       return (
-        <div key={rowIndex} className="column">
-          {cols}
+        <div key={rowIndex} className="row">
+          {rows}
         </div>
       );
     });
@@ -24,23 +24,40 @@ class Game extends React.Component {
   constructor() {
     super();
     this.state = {
-      board: [['*', '*', '*'], ['*', '*', '*'], ['*', '*', '*']],
+      board: [[null, '', ' '], [' ', null, ''], [' ', '', null]],
       turn: 0,
+      player: 'X',
+      win: false,
     };
   }
 
   squareClick = (row, col) => {
-    console.log('row:', row, 'col:', col);
+    console.log(row, col);
     let updateBoard = this.state.board.slice(0);
-    this.state.turn % 2 == 0 ? (updateBoard[col][row] = 'X') : (updateBoard[col][row] = 'O');
+    let increment = this.state.turn;
+    let currentplayer = this.state.player;
+    this.state.turn % 2 == 0 ? (currentplayer = 'X') : (currentplayer = 'O');
+    updateBoard[row][col] = currentplayer;
+    increment++;
+    this.setState({ turn: increment });
     this.setState({ board: updateBoard });
-    this.state.turn++;
+    this.setState({ player: currentplayer });
+    if (this.state.turn >= 4) {
+      if (checkWin(updateBoard)) {
+        let winner = this.state.win;
+        winner = true;
+        this.setState({ win: winner });
+      }
+    }
   };
 
   render() {
+    let status = <h1>ReacticTacToe</h1>;
+    if (this.state.win) status = <h1>{this.state.player} WINS!</h1>;
     return (
       <div className="game">
         <Board board={this.state.board} squareClick={this.squareClick} />
+        <div className="text-center m-2">{status}</div>
       </div>
     );
   }
