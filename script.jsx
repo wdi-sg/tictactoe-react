10542e57f132e894e62e8963737daffc1042102c
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-11-08 16:24:40 +0800

    Further 1: Create game component

--- script.jsx
+++ script.jsx
@@ -1,27 +1,11 @@
 class Board extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      board: [['*', '*', '*'], ['*', '*', '*'], ['*', '*', '*']],
-      turn: 0,
-    };
-  }
-
-  squareClick = (row, col) => {
-    console.log('row:', row, 'col:', col);
-    let updateBoard = this.state.board.slice(0);
-    this.state.turn % 2 == 0 ? (updateBoard[col][row] = 'X') : (updateBoard[col][row] = '0');
-    this.setState({ board: updateBoard });
-    this.state.turn++;
-  };
-
   render() {
-    const board = this.state.board.map((row, rowIndex) => {
+    const board = this.props.board.map((row, rowIndex) => {
       const cols = row.map((col, colIndex) => (
         <div
           key={colIndex}
           className="square d-flex flex-column justify-content-center align-items-center"
-          onClick={() => this.squareClick(colIndex, rowIndex)}
+          onClick={() => this.props.squareClick(colIndex, rowIndex)}
         >
           <span className="display-3 font-weight-bold">{col}</span>
         </div>
@@ -36,10 +20,36 @@ class Board extends React.Component {
   }
 }
 
+class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      board: [['*', '*', '*'], ['*', '*', '*'], ['*', '*', '*']],
+      turn: 0,
+    };
+  }
+
+  squareClick = (row, col) => {
+    console.log('row:', row, 'col:', col);
+    let updateBoard = this.state.board.slice(0);
+    this.state.turn % 2 == 0 ? (updateBoard[col][row] = 'X') : (updateBoard[col][row] = 'O');
+    this.setState({ board: updateBoard });
+    this.state.turn++;
+  };
+
+  render() {
+    return (
+      <div className="game">
+        <Board board={this.state.board} squareClick={this.squareClick} />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
   <div className="container-fluid h-100">
     <div className="d-flex h-100 flex-column align-items-center justify-content-center">
-      <Board />
+      <Game />
     </div>
   </div>,
   document.getElementById('root'),
