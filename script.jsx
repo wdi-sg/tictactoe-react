75fcaed4c2343945cc6fd276d45745c4e8720f49
angrylobster <liewminshan@gmail.com>
2019-02-14 23:24:41 +0800

    Refactor code to use cell numbers and implement win check

--- script.jsx
+++ script.jsx
@@ -3,88 +3,100 @@ class Board extends React.Component {
         super();
         this.turnTaken = this.turnTaken.bind(this)
         this.state = {
-            board: [
-                ['','',''],
-                ['','',''],
-                ['','','']
-            ],
-            turns: 0
+            turns: 0,
+            length: 3,
+            playerXMoves: [],
+            playerOMoves: [] 
         }
-        this.state.victories = this.generateVictories()
+        this.state.victories = this.generateVictories();
+        this.state.board = this.generateBoard();
+    }
+
+    generateBoard(){
+        let board = [];
+        for(let rows = 0; rows < this.state.length; rows++){
+            for (let columns = 0; columns < this.state.length; columns++){
+                board.push('')
+            }
+        }
+        return board;
     }
 
     generateVictories(){
         let victories = [];
-        for (let i = 0; i < this.state.board.length; i++){
-            let rowVictory = [];
-            let columnVictory = [];
-            if (i === 0){
-                let diagonalVictory = [];
-                for (let k = 0; k < this.state.board.length; k++){
-                    diagonalVictory.push([k,k])
-                }
-                victories.push(diagonalVictory)
+
+        for (let i = 0; i < Math.pow(this.state.length,2); i+= this.state.length){
+            let row = [];
+            for (let j = 0; j < this.state.length; j++){
+                row.push(i+j)
             }
-            if (i === this.state.board.length - 1){
-                let diagonalVictory = [];
-                for (let k = this.state.board.length - 1; k >= 0; k--){
-                    diagonalVictory.push([k,k])
-                }
-                victories.push(diagonalVictory)
-            }
-            for (let j = 0; j < this.state.board.length; j++){
-                rowVictory.push([j,i])
-                columnVictory.push([i,j])
-            }
-            victories.push(rowVictory)
-            victories.push(columnVictory)
+            victories.push(row)
         }
-        return victories;
+
+        for (let i = 0; i < this.state.length; i++){
+            let column = [];
+            for (let j = 0; j < Math.pow(this.state.length,2); j+= this.state.length){
+                column.push(i+j)
+            }
+            victories.push(column)
+        }
+        
+        let leftRightDiagonal = [];
+        let rightLeftDiagonal = [];
+        for (let i = 0; i < this.state.length; i++){
+            leftRightDiagonal.push((i*this.state.length)+i)
+            rightLeftDiagonal.push((this.state.length-1)*(1+i))
+        }
+        victories.push(leftRightDiagonal)
+        victories.push(rightLeftDiagonal)
+        return victories
     }
 
     turnTaken(cell){
         this.occupyCell(cell);
         this.setState({ turns: this.state.turns + 1 });
-        // this.checkVictory();
+        this.checkVictory();
+    }
+
+    checkVictory(){
+        this.state.victories.forEach(victory => {
+            if (victory.every(cell => {
+                return this.state.playerXMoves.includes(cell)
+            })){
+                alert("X Won!")
+            } else if (victory.every(cell => {
+                return this.state.playerOMoves.includes(cell)
+            })){
+                alert("O Won!")
+            }
+        })
     }
 
     occupyCell(cell){
         let currentBoard = this.state.board;
         if (this.state.turns % 2 === 0) {
-            currentBoard[cell.row][cell.col] = 'X'
+            currentBoard[cell.cellIndex] = 'X'
+            this.state.playerXMoves.push(cell.cellIndex)
         } else {
-            currentBoard[cell.row][cell.col] = 'O'
+            currentBoard[cell.cellIndex] = 'O'
+            this.state.playerOMoves.push(cell.cellIndex)
         }
         this.setState({ board: currentBoard })
     }
 
     render() {
-        console.log(this.state.victories)
-        const board = this.state.board.map( (row,rowIndex) => {
-        // make a single row
-            const rows = row.map( (col,colIndex) => {
-            // make each column
-                return (
-                    <p 
-                        className="btn btn-outline-primary mr-2 mb-2"
-                        style={{width: 50, height: 50}}
-                        key={colIndex}
-                        onClick={() => { 
-                            this.turnTaken({
-                                col: colIndex,
-                                row: rowIndex
-                            }) 
-                        }}
-                    >
-                        {this.state.board[rowIndex][colIndex]}
-                    </p>
-                );
-            });
+        const board = this.state.board.map((cell,cellIndex) => {
             return (
-                <div key={rowIndex} className="text-center">
-                    {rows}
-                </div>
-            );
+                <p 
+                    className="btn btn-outline-primary mr-2 mb-2 pt-5"
+                    style={{ width: 100, height: 100 }}
+                    key={cellIndex}
+                    onClick={() => { 
+                        this.turnTaken({ cellIndex: cellIndex }) 
+                    }}>
+                    {cell}
+                </p>
+            )
         });
         return (
             <div className="container w-50 h-50 mt-5">
