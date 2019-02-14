875ce3d3b9720f0c357674bdf7701a5ba85cc087
sean chan <sean@seans-MacBook-Air.local>
2019-02-14 20:04:03 +0800

    Further 1

--- script.jsx
+++ script.jsx
@@ -1,104 +1,3 @@
-// class Board extends React.Component {
-
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//           squares: Array(9).fill(null),
-//           nextX: true
-//         };
-//     }
-
-//     handleClick() {
-//         console.log(this)
-//         if(this.state.nextX == true){
-//             this.setState({nextX: false})
-//         }
-//         else if(this.state.nextX == false){
-//             this.setState({nextX: true})
-//         }
-//     }
-
-//     renderSquare(i) {
-//         return (
-//           <Square value={this.state.squares[i]} onClick={this.handleClick}/>
-//         );
-//     }
-
-//     render() {
-//         const status = 'Next player: X';
-//         return (
-//             <div className="board">
-//                 <div className="status">{status}</div>
-//                 <div className="board-row">
-//                 {this.renderSquare(0)}
-//                 {this.renderSquare(1)}
-//                 {this.renderSquare(2)}
-//                 </div>
-//                 <div className="board-row">
-//                 {this.renderSquare(3)}
-//                 {this.renderSquare(4)}
-//                 {this.renderSquare(5)}
-//                 </div>
-//                 <div className="board-row">
-//                 {this.renderSquare(6)}
-//                 {this.renderSquare(7)}
-//                 {this.renderSquare(8)}
-//                 </div>
-//             </div>
-//             );
-//     }
-// }
-
-// class Square extends React.Component {
-
-//     constructor(props) {
-//         super(props);
-//         this.handleClick = this.handleClick.bind(this);
-//         this.state = {
-//           value: null,
-//           nextX: true,
-//       };
-//     }
-
-//     handleClick(){
-//         // console.log(this.state.nextX)
-//         if(this.state.nextX == true){
-//             this.setState({value: 'X'})
-//         }
-//         else if(this.state.nextX == false){
-//             this.setState({value: 'O'})
-//         }
-//     }
-
-//     render() {
-//         return (
-//             <button className="square" onClick={this.handleClick}>
-//                 {this.state.value}
-//             </button>
-//         );
-//     }
-// }
-
-// class Game extends React.Component {
-//     render() {
-//         return (
-//           <div className="game">
-//             <div className="game-board">
-//                 <Board />
-//             </div>
-//             <div className="game-info">
-//             </div>
-//           </div>
-//         );
-//     }
-// }
-
-// ReactDOM.render(
-//     <Board/>,
-//     document.getElementById('root')
-//     );
-
-
 class Board extends React.Component {
 
     constructor(props) {
@@ -136,7 +35,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        let status = 'Current player: X'
+        if(this.state.nextX == true){
+            status = 'Current player: X';
+        }
+        else{
+            status = 'Current player: O';
+        }
         return (
             <div className="board">
                 <div className="status">{status}</div>
@@ -183,6 +88,6 @@ class Game extends React.Component {
 }
 
 ReactDOM.render(
-    <Board/>,
+    <Game/>,
     document.getElementById('root')
     );
