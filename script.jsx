382099b8ee76e6a2437e1b0c68b7a561998e6a70
rchuaml <ronchuaml@gmail.com>
2019-02-14 17:04:54 +0800

    added mvp and win conditions

--- script.jsx
+++ script.jsx
@@ -5,21 +5,51 @@ class Board extends React.Component {
 
       this.state = {
         board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
-        ]
+          [""," ","  "],
+          ["   ","    ","     "],
+          ["      ","       ","         "],
+        ],
+        counter : 0,
+        playerturn: ""
       }
-
+    this.squareClick = this.squareClick.bind(this);
     }
 
-    squareClick(something){
-        console.log( something );
+
+
+    squareClick(row, column){
+    let currentTurn = this.state.counter + 1;
+      this.setState( { counter: currentTurn } );
+        if (this.state.counter%2 === 0 ){
+            this.state.board[row][column] = "X"
+        }else if (this.state.counter%2 !== 0){
+            this.state.board[row][column] = "O"
+        }
+        this.checkWin(row,column);
+    }
+
+//check win conditions
+    checkWin(row, column){
+        if(this.state.board[0][0] == this.state.board[0][1] && this.state.board[0][1] == this.state.board[0][2]){
+            alert("player " + this.state.playerturn + " wins!");
+        } else if(this.state.board[1][0] == this.state.board[1][1] && this.state.board[1][1] == this.state.board[1][2]){
+            alert("player " + this.state.playerturn + " wins!");
+        } else if(this.state.board[2][0] == this.state.board[2][1] && this.state.board[2][1] == this.state.board[2][2]){
+            alert("player " + this.state.playerturn + " wins!");
+        } else if(this.state.board[0][0] == this.state.board[1][0] && this.state.board[1][0] == this.state.board[2][0]){
+            alert("player " + this.state.playerturn + " wins!");
+        } else if(this.state.board[0][1] == this.state.board[1][1] && this.state.board[1][1] == this.state.board[1][2]){
+            alert("player " + this.state.playerturn + " wins!");
+        } else if(this.state.board[0][2] == this.state.board[1][2] && this.state.board[1][2] == this.state.board[2][2]){
+            alert("player " + this.state.playerturn + " wins!");
+        } else if(this.state.board[0][0] == this.state.board[1][1] && this.state.board[1][1] == this.state.board[2][2]){
+            alert("player " + this.state.playerturn + " wins!");
+        } else if(this.state.board[0][2] == this.state.board[1][1] && this.state.board[2][0] == this.state.board[0][2]){
+            alert("player " + this.state.playerturn + " wins!");
+        }
     }
 
     render() {
-        console.log("board", this.state.board);
-
         const board = this.state.board.map( (row,rowIndex) => {
 
           // make a single row
@@ -27,16 +57,12 @@ class Board extends React.Component {
 
             // make each column
             return (
-                    <p
-                        className="boo"
+                    <button className="boo"
                         key={colIndex}
                         onClick={()=>{
-                            this.squareClick(colIndex);
-                        }}
-
-                    >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
+                            this.squareClick(rowIndex,colIndex);
+                        }}>{this.state.board[rowIndex][colIndex]}
+                    </button>
             );
 
           });
@@ -59,7 +85,18 @@ class Board extends React.Component {
     }
 }
 
+// class Game extends React.Component {
+
+//     render() {
+//         return (
+//           <div>
+//             <Board board={this.state.board}/>
+//           </div>
+//         );
+//     }
+// }
+
 ReactDOM.render(
-    <Board/>,
+        <Board/>,
     document.getElementById('root')
 );
