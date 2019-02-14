5f3b13bfe4a6a68445a83251808ba97ea034fe8d
SiewLeng <siewleng.lim@gmail.com>
2019-02-14 23:22:09 +0800

    check winning

--- script.jsx
+++ script.jsx
@@ -8,17 +8,175 @@ class Board extends React.Component {
           ['i','i','i'],
           ['i','i','i'],
           ['i','i','i']
-        ]
+        ],
+
+        counter: 1,
+
+        message: ["Player X"],
+
+        resume: true
+
       }
+
+      this.squareClick = this.squareClick.bind(this);
 
     }
 
-    squareClick(something){
-        console.log( something );
+    checkRow(letter) {
+        let tempBoard = [...this.state.board];
+        for (let row = 0; row < tempBoard.length; row++) {
+            for (let col = 0; col < tempBoard.length - 2; col++) {
+                let num = 0;
+                for (let i = 0; i < 3; i++) {
+                    if (tempBoard[row][col+i] == letter) {
+                        num = num + 1;
+                    }
+                }
+                if (num == 3) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    checkCol(letter) {
+        let tempBoard = [...this.state.board];
+        for (let col = 0; col < tempBoard.length; col++) {
+            for (let row = 0; row < tempBoard.length - 2; row++) {
+                let num = 0;
+                for (let i = 0; i < 3; i++) {
+                    if (tempBoard[row + i][col] == letter) {
+                        num = num + 1;
+                    }
+                }
+                if (num == 3) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    checkDiagonal1(letter) {
+        let tempBoard = [...this.state.board];
+        for (let row = 0; row < tempBoard.length; row++) {
+            for (let col = 0; col < tempBoard.length; col++) {
+                let num = 0;
+                for (let i = 0; i < 3; i++) {
+                    if ((row + i) < tempBoard.length && (col + i) < tempBoard.length && tempBoard[row + i][col + i] == letter) {
+                        num = num + 1;
+                    }
+                }
+                if (num == 3) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    checkDiagonal2(letter) {
+        let tempBoard = [...this.state.board];
+        for (let row = 0; row < tempBoard.length; row++) {
+            for (let col = 0; col < tempBoard.length; col++) {
+                let num = 0;
+                for (let i = 0; i < 3; i++) {
+                    if ((row + i) < tempBoard.length && (col - i) < tempBoard.length && tempBoard[row + i][col - i] == letter) {
+                        num = num + 1;
+                    }
+                }
+                if (num == 3) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    winning(letter) {
+        if (this.checkRow(letter)) {
+            return true;
+        }
+
+        else if (this.checkCol(letter)) {
+            return true;
+        }
+
+        else if (this.checkDiagonal1(letter)) {
+            return true;
+        }
+
+        else if (this.checkDiagonal2(letter)) {
+            return true;
+        }
+
+        else  {
+            return false;
+        }
+    }
+
+    checkStatus(letter) {
+        let tempMessage = [...this.state.message];
+        if (this.winning(letter)) {
+            tempMessage.push("Player " + letter + " is winner");
+            this.setState( { message: tempMessage} );
+            return true;
+        }
+        else if (this.state.counter == 9) {
+            tempMessage.push("It is a draw");
+            this.setState( { message: tempMessage} );
+            return true;
+        }
+        else {
+            if (letter == 'X') {
+                tempMessage.push("Player O");
+                this.setState( { message: tempMessage} );
+                return false;
+
+            }
+            else  {
+                tempMessage.push("Player X");
+                this.setState( { message: tempMessage} );
+                return false;
+            }
+        }
+    }
+
+
+    squareClick(X, Y){
+        let tempBoard = [...this.state.board];
+
+        if (this.state.resume && this.state.counter % 2 != 0 && tempBoard[X][Y] == 'i') {
+            let currentCounter = this.state.counter + 1;
+            tempBoard[X][Y] = 'X';
+
+            this.setState( { board: tempBoard} );
+            this.setState( { counter: currentCounter} );
+
+            if (this.checkStatus(tempBoard[X][Y])) {
+                let currentResume = !this.state.resume;
+                this.setState( { resume: currentResume} );
+            }
+        }
+
+        if (this.state.resume && this.state.counter % 2 == 0 && tempBoard[X][Y] == 'i') {
+            let currentCounter = this.state.counter + 1;
+            tempBoard[X][Y] = 'O';
+
+            this.setState( { board: tempBoard} );
+            this.setState( { counter: currentCounter} );
+
+            if (this.checkStatus(tempBoard[X][Y])) {
+                let currentResume = !this.state.resume;
+                this.setState( { resume: currentResume} );
+            }
+        }
     }
 
     render() {
-        console.log("board", this.state.board);
+        console.log("rendering");
 
         const board = this.state.board.map( (row,rowIndex) => {
 
@@ -27,16 +185,16 @@ class Board extends React.Component {
 
             // make each column
             return (
-                    <p
+                    <button
                         className="boo"
                         key={colIndex}
                         onClick={()=>{
-                            this.squareClick(colIndex);
+                            this.squareClick(rowIndex, colIndex);
                         }}
 
                     >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
+                        {col}
+                    </button>
             );
 
           });
@@ -52,9 +210,10 @@ class Board extends React.Component {
         });
 
         return (
-          <div className="item">
-            {board}
-          </div>
+            <div className="item">
+                <p> {this.state.message[this.state.message.length-1]} </p>
+                {board}
+            </div>
         );
     }
 }
