d8e870333293d0e29b90ff9aa65dc2b4ed582c81
angrylobster <liewminshan@gmail.com>
2019-02-14 21:13:03 +0800

    Generate victories based on board size

--- script.jsx
+++ script.jsx
@@ -10,30 +10,64 @@ class Board extends React.Component {
             ],
             turns: 0
         }
+        this.state.victories = this.generateVictories()
+    }
+
+    generateVictories(){
+        let victories = [];
+        for (let i = 0; i < this.state.board.length; i++){
+            let rowVictory = [];
+            let columnVictory = [];
+            if (i === 0){
+                let diagonalVictory = [];
+                for (let k = 0; k < this.state.board.length; k++){
+                    diagonalVictory.push([k,k])
+                }
+                victories.push(diagonalVictory)
+            }
+            if (i === this.state.board.length - 1){
+                let diagonalVictory = [];
+                for (let k = this.state.board.length - 1; k >= 0; k--){
+                    diagonalVictory.push([k,k])
+                }
+                victories.push(diagonalVictory)
+            }
+            for (let j = 0; j < this.state.board.length; j++){
+                rowVictory.push([j,i])
+                columnVictory.push([i,j])
+            }
+            victories.push(rowVictory)
+            victories.push(columnVictory)
+        }
+        return victories;
     }
 
     turnTaken(cell){
-        console.log(cell)
-        this.cellOccupied(cell)
-        this.setState({ turns: this.state.turns + 1 })
+        this.occupyCell(cell);
+        this.setState({ turns: this.state.turns + 1 });
+        // this.checkVictory();
     }
 
-    cellOccupied(cell){
+    occupyCell(cell){
         let currentBoard = this.state.board;
-        currentBoard[cell.row][cell.col] = 'Y'
+        if (this.state.turns % 2 === 0) {
+            currentBoard[cell.row][cell.col] = 'X'
+        } else {
+            currentBoard[cell.row][cell.col] = 'O'
+        }
         this.setState({ board: currentBoard })
     }
 
     render() {
-        
-        console.log(this.state.board);
+        console.log(this.state.victories)
         const board = this.state.board.map( (row,rowIndex) => {
         // make a single row
             const rows = row.map( (col,colIndex) => {
             // make each column
                 return (
                     <p 
-                        className="btn btn-outline-primary mr-2"
+                        className="btn btn-outline-primary mr-2 mb-2"
+                        style={{width: 50, height: 50}}
                         key={colIndex}
                         onClick={() => { 
                             this.turnTaken({
