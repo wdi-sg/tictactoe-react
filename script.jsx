25196a6c1b0884e963ba9c16b04c278da889595a
kingcashthefifth <kingcashthefifth@gmail.com>
2019-02-14 16:01:21 +0800

    react states working to change X and O on click, alert if box used already

--- script.jsx
+++ script.jsx
@@ -1,65 +1,63 @@
 class Board extends React.Component {
-    constructor(){
+  constructor() {
+    super();
 
-      super()
+    this.state = {
+      board: [["i", "i", "i"], ["i", "i", "i"], ["i", "i", "i"]],
+      turn: 0
+    };
+    this.squareClick = this.squareClick.bind(this);
+  }
 
-      this.state = {
-        board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
-        ]
-      }
-
+  squareClick(row, col) {
+    console.log("starting", this.state.board);
+    let updateBoard = this.state.board;
+    let updateTurn = this.state.turn;
+    if (this.state.turn % 2 == 0 && updateBoard[row][col] == "i") {
+      updateBoard[row][col] = "X";
+      updateTurn = this.state.turn + 1;
+    } else if (this.state.turn % 2 !== 0 && updateBoard[row][col] == "i") {
+      updateBoard[row][col] = "O";
+      updateTurn = this.state.turn + 1;
+    } else {
+      alert("Box is taken, please choose another box.");
     }
+    console.log("ending", updateBoard);
+    const newState = { board: updateBoard, turn: updateTurn };
+    this.setState(newState);
+    console.log("Board updated, turn is now: ", updateTurn);
+  }
 
-    squareClick(something){
-        console.log( something );
-    }
+  render() {
+    console.log("board", this.state.board);
 
-    render() {
-        console.log("board", this.state.board);
-
-        const board = this.state.board.map( (row,rowIndex) => {
-
-          // make a single row
-          const rows = row.map( (col,colIndex) => {
-
-            // make each column
-            return (
-                    <p
-                        className="boo"
-                        key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex);
-                        }}
-
-                    >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
-            );
-
-          });
-
-          // return the complete row
-          return (
-            <div key={rowIndex} className="row">
-              {rows}
-            </div>
-
-          );
-
-        });
-
+    const board = this.state.board.map((row, rowIndex) => {
+      // make a single row
+      const rows = row.map((col, colIndex) => {
+        // make each column
         return (
-          <div className="item">
-            {board}
-          </div>
+          <button
+            className="boo"
+            key={colIndex}
+            onClick={() => {
+              this.squareClick(rowIndex, colIndex);
+            }}
+          >
+            {col}
+          </button>
         );
-    }
+      });
+
+      // return the complete row
+      return (
+        <div key={rowIndex} className="row">
+          {rows}
+        </div>
+      );
+    });
+
+    return <div className="item">{board}</div>;
+  }
 }
 
-ReactDOM.render(
-    <Board/>,
-    document.getElementById('root')
-);
+ReactDOM.render(<Board />, document.getElementById("root"));
