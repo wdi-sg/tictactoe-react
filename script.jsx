4c2ad3f0cbb46aef31034a1070035e5d008c9a04
tengchoonhong90 <tengchoonhong_90@outlook.com>
2019-02-14 15:47:04 +0800

    initial commit

--- script.jsx
+++ script.jsx
@@ -1,65 +1,97 @@
 class Board extends React.Component {
-    constructor(){
+  constructor() {
+    super()
 
-      super()
+    this.playerOne = {
+      symbol: 'O'
+    }
 
-      this.state = {
-        board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
-        ]
+    this.playerTwo = {
+      symbol: 'X'
+    }
+
+    this.state = {
+      board: [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+      ],
+      currentPlayer: this.playerOne 
+    }
+
+  }
+
+  squareClick(colIndex, rowIndex) {
+    console.log("colIndex: " + colIndex,"rowIndex: " + rowIndex);
+    if (this.state.board[rowIndex][colIndex] == ' ') {
+      var newBoard = this.state.board;
+
+      console.log("symbol: " + this.state.currentPlayer.symbol)
+      newBoard[rowIndex][colIndex] = this.state.currentPlayer.symbol
+
+      let nextPlayer;
+      if (this.state.currentPlayer == this.playerOne) {
+        nextPlayer = this.playerTwo;
+      } else {
+        nextPlayer = this.playerOne;
       }
-
+      this.setState({
+        board: newBoard,
+        currentPlayer: nextPlayer
+      })
     }
+  }
 
-    squareClick(something){
-        console.log( something );
-    }
+  render() {
+    console.log("board", this.state.board);
 
-    render() {
-        console.log("board", this.state.board);
+    const board = this.state.board.map((row, rowIndex) => {
 
-        const board = this.state.board.map( (row,rowIndex) => {
+      // make a single row
+      const rows = row.map((col, colIndex) => {
 
-          // make a single row
-          const rows = row.map( (col,colIndex) => {
-
-            // make each column
-            return (
-                    <p
-                        className="boo"
-                        key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex);
-                        }}
-
-                    >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
-            );
-
-          });
-
-          // return the complete row
-          return (
-            <div key={rowIndex} className="row">
-              {rows}
-            </div>
-
-          );
-
-        });
-
+        // make each column
         return (
-          <div className="item">
-            {board}
-          </div>
+          <p className="square" key={colIndex} 
+            onClick={() => {
+              this.squareClick(colIndex, rowIndex);
+            }}
+          >
+            {col}
+          </p>
         );
-    }
+
+      });
+
+      // return the complete row
+      return (
+        <div key={rowIndex} className="row">
+          {rows}
+        </div>
+
+      );
+
+    });
+
+    return (
+      <div className="item">
+        {board}
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render () {
+    return (
+      <div id="board">
+      <Board />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
-    <Board/>,
-    document.getElementById('root')
+  <Game />,
+  document.getElementById('root')
 );
