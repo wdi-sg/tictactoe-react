5c9198f75b93de2d5c96db3fc70e74990fb09477
shiowwj <shiowweijun@gmail.com>
2019-05-14 20:02:06 +0800

    Added winning state

--- script.jsx
+++ script.jsx
@@ -21,29 +21,67 @@ class Board extends React.Component {
     clicker(event,colIndex,rowIndex){
         let eks = 'X';
         let ouu = 'O';
-        let gameBoard;
-
         // console.log(this.turn)
         if(this.turn%2 == 0 && this.state.board[colIndex][rowIndex] === '-'){
-            gameBoard = this.state.board;
-            gameBoard[colIndex][rowIndex] = eks;
-            this.setState({board: gameBoard});
+            this.state.board[colIndex][rowIndex] = eks;
+            this.setState({board: this.state.board});
             this.turn++ ;
+            this.checkWinState(this.turn,colIndex,rowIndex,eks);
         } else if (this.turn%2 == 1 && this.state.board[colIndex][rowIndex] === '-'){
-            gameBoard = this.state.board;
-            gameBoard[colIndex][rowIndex] = ouu;
-            this.setState({board: gameBoard});
+            this.state.board[colIndex][rowIndex] = ouu;
+            this.setState({board: this.state.board});
             this.turn++ ;
+            this.checkWinState(this.turn,colIndex,rowIndex,ouu);
         }
-        console.log(this.turn)
-        this.checkWinState(this.turn);
     }
 
-    checkWinState(turn){
+    checkWinState(turn,colIndex,rowIndex,playerSymbol){
+        let n = 3;
         console.log('checking...');
-        // if(this.turn === 9 ){
-        //     console.log('draw');
-        // }
+        // console.log(turn);
+        // console.log(colIndex+ ' ' +rowIndex);
+        // console.log(playerSymbol);
+        //check col game win condition
+        for(let i = 0; i < n ;i++){
+            if(this.state.board[colIndex][i] !== playerSymbol)
+                break;
+            if (i === n-1){
+                // console.log('somebody wins')
+                alert(`${playerSymbol} won`)
+            }
+        }
+        //check row game win condition
+        for(let i = 0; i < n; i++){
+            if(this.state.board[i][rowIndex] !== playerSymbol)
+                break;
+            if(i === n-1){
+                alert(`${playerSymbol} won`)
+            }
+        }
+        //check diag
+        if(colIndex === rowIndex){
+            for(let i = 0; i < n; i++){
+                if(this.state.board[i][i] !== playerSymbol)
+                    break;
+                if(i == n-1){
+                    alert(`${playerSymbol} won`)
+                }
+            }
+        }
+        //check reverse diag
+        if(colIndex+colIndex === n-1){
+            for(let i = 0; i < n; i++){
+                if(this.state.board[i][(n-1)-i] !== playerSymbol)
+                    break;
+                if(i == n-1){
+                    alert(`${playerSymbol} won`)
+                }
+            }
+        }
+        //check draw state
+        if(turn == (Math.pow(n,2))){
+            alert(`This is a DRAW`);
+        }
     }
 
     render() {
