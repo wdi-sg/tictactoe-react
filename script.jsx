fed7c7256f1bff864b7f4e6bf859c187c3d1374e
moses <moseslee90@gmail.com>
2019-05-14 16:52:38 +0800

    click handler is changing state and dom of correct elements and variables

--- script.jsx
+++ script.jsx
@@ -1,65 +1,49 @@
 class Board extends React.Component {
-    constructor(){
+  constructor() {
+    super();
 
-      super()
+    this.state = {
+      board: [["i", "i", "i"], ["i", "i", "i"], ["i", "i", "i"]]
+    };
+  }
 
-      this.state = {
-        board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
-        ]
-      }
+  squareClick(col, row) {
+    console.log(col + "-" + row);
+    console.log(this.state.board[row][col]);
+    this.state.board[row][col] = 0;
+    let newState = this.state;
+    this.setState(newState);
+  }
 
-    }
+  render() {
+    console.log("board", this.state.board);
 
-    squareClick(something){
-        console.log( something );
-    }
-
-    render() {
-        console.log("board", this.state.board);
-
-        const board = this.state.board.map( (row,rowIndex) => {
-
-          // make a single row
-          const rows = row.map( (col,colIndex) => {
-
-            // make each column
-            return (
-                    <p
-                        className="boo"
-                        key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex);
-                        }}
-
-                    >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
-            );
-
-          });
-
-          // return the complete row
-          return (
-            <div key={rowIndex} className="row">
-              {rows}
-            </div>
-
-          );
-
-        });
-
+    const board = this.state.board.map((row, rowIndex) => {
+      // make a single row
+      const rows = row.map((col, colIndex) => {
+        // make each column
         return (
-          <div className="item">
-            {board}
+          <div
+            className="boo"
+            key={colIndex}
+            onClick={() => {
+              this.squareClick(colIndex, rowIndex);
+            }}>
+            {col} : {colIndex} : {rowIndex}
           </div>
         );
-    }
+      });
+
+      // return the complete row
+      return (
+        <div key={rowIndex} className="row">
+          <div className="col d-flex justify-content-between">{rows}</div>
+        </div>
+      );
+    });
+
+    return <div className="item container">{board}</div>;
+  }
 }
 
-ReactDOM.render(
-    <Board/>,
-    document.getElementById('root')
-);
+ReactDOM.render(<Board />, document.getElementById("root"));
