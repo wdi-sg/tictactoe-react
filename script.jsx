b03a06cc6a2cd5639f8650125ef47e5893d6f14a
seeo <aragonlotr5@gmail.com>
2019-05-14 19:00:56 +0800

    changed the constructor to be at the board component (parent), rather than at the square component, so that passing states can be easier

--- script.jsx
+++ script.jsx
@@ -1,28 +1,44 @@
 class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
+    //delete costructor from the square component since square no longer keeps track of the game state, but board is.
     render() {
         console.log("We have generated/clicked on square...", this.props.value);
         //By calling this.setState from an onClick handler in the Square’s render method, we tell React to re-render that Square whenever its <button> is clicked.
         return (
             <button
             className="square"
-            onClick={() =>this.setState({value: 'X'})}
+            onClick={() => this.props.onClick()}
             >
-                {this.state.value}
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+/*To collect data from multiple children,
+or to have two child components communicate with each other,
+you need to declare the shared state in their parent component instead.
+The parent component can pass the state back down to the children by using props;
+this keeps the child components in sync with each other and with the parent component.
+*/
+
+constructor(props){
+/*Add a constructor to the Board and set the Board’s initial state to contain an array of 9 nulls;
+corresponding to the 9 squares:
+  */
+    super(props);
+    this.state = {
+        squares: Array(9).fill(null),
+    }
+}
     renderSquare(i) {
-        return <Square value={i} />;
+        /* Now we’re passing down two props from Board to Square: value and onClick.
+        We could not set it at the Square component; if we did, we would not be able to pass it back up to the board.
+        Since state is considered to be private to a component that defines it, we cannot update the Board’s state directly from Square.*/
+        return <Square
+            value={this.state.squares[i]}
+            onClick={()=>this.handleClick(i)}
+        />;
     }
 
     render() {
