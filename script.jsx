c67999c9f6149dafde8f5f0c14fa814d3f77c5d5
seeo <aragonlotr5@gmail.com>
2019-05-14 21:10:11 +0800

    working on a history tracker that I am placing in the Game component, that is above the Board component

--- script.jsx
+++ script.jsx
@@ -93,6 +93,15 @@ corresponding to the 9 squares:
 }
 
 class Game extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            history:[{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        };
+    })
     render() {
         return (
             <div className = "game">
