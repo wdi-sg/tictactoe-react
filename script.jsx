484e06afe7dfb774929c8d88235798fbbf8d39c3
shiowwj <shiowweijun@gmail.com>
2019-05-14 19:28:57 +0800

    Allowed state change and turn counter

--- script.jsx
+++ script.jsx
@@ -1,42 +1,65 @@
+class Square extends React.Component{
+
+}
+
 class Board extends React.Component {
     constructor(){
+    super()
+    this.turn = 0;
 
-      super()
 
-      this.state = {
+    this.state = {
         board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
+          ['-','-','-'],
+          ['-','-','-'],
+          ['-','-','-']
         ]
-      }
+    }
 
     }
 
-    squareClick(something){
-        console.log( something );
+    clicker(event,colIndex,rowIndex){
+        let eks = 'X';
+        let ouu = 'O';
+        let gameBoard;
+
+        // console.log(this.turn)
+        if(this.turn%2 == 0 && this.state.board[colIndex][rowIndex] === '-'){
+            gameBoard = this.state.board;
+            gameBoard[colIndex][rowIndex] = eks;
+            this.setState({board: gameBoard});
+            this.turn++ ;
+        } else if (this.turn%2 == 1 && this.state.board[colIndex][rowIndex] === '-'){
+            gameBoard = this.state.board;
+            gameBoard[colIndex][rowIndex] = ouu;
+            this.setState({board: gameBoard});
+            this.turn++ ;
+        }
+        console.log(this.turn)
+        this.checkWinState(this.turn);
+    }
+
+    checkWinState(turn){
+        console.log('checking...');
+        // if(this.turn === 9 ){
+        //     console.log('draw');
+        // }
     }
 
     render() {
         console.log("board", this.state.board);
-
+        //doing a nested loop
         const board = this.state.board.map( (row,rowIndex) => {
 
           // make a single row
           const rows = row.map( (col,colIndex) => {
-
+            let startBox = ' ';
             // make each column
             return (
-                    <p
-                        className="boo"
-                        key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex);
-                        }}
+                    <button type="checkbox" className="box" key={colIndex} onClick={((ev)=> this.clicker(ev,colIndex, rowIndex))}>
+                        {this.state.board[colIndex][rowIndex]}
+                    </button>
 
-                    >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
             );
 
           });
