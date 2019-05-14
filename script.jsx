5aa27a11e3f17c1d239419a823b0a94529912f9a
reynosoturtle <christianreynoso96@gmail.com>
2019-05-14 23:09:09 +0800

    added match draw logic

--- script.jsx
+++ script.jsx
@@ -9,38 +9,54 @@ class Board extends React.Component {
           ['fuck','fuck','fuck'],
           ['fuck','fuck','fuck']
         ],
-        turn: "mother"
+        turn: "mother",
+        spots: 9
       }
 
     }
 
 
     squareClick(rowIndex, colIndex){
-
+      let checkForDraw = function (value) {
+        // console.log("old spots", this.state.spots)
+        // this.setState
+        // console.log("new draw:", draw)
+        if (value == 0) {
+          alert('draw')
+        }
+      }
 
       let winConditions = function () {
 
       }
 
 
-      console.log( rowIndex, colIndex );
-      console.log(this.state.board[rowIndex][colIndex])
+      // console.log( rowIndex, colIndex );
+      // console.log(this.state.board[rowIndex][colIndex])
       const dupBoard = this.state.board
       const turn = document.getElementById('turn')
+      let newSpots;
       // this.state.board[rowIndex][colIndex] = "ur mader"
       if (this.state.board[rowIndex][colIndex] == "fuck") {
         if ( turn.innerHTML == "mother") {
           dupBoard[rowIndex][colIndex] = "ur mader"
-          this.setState({ board: dupBoard, turn: "father" })
+          newSpots = this.state.spots-1
+          this.setState({ board: dupBoard, turn: "father", spots: newSpots  })
+          checkForDraw(newSpots)
         } else {
           dupBoard[rowIndex][colIndex] = "ur father"
-          this.setState({ board: dupBoard, turn: "mother"  })
+          newSpots = this.state.spots-1
+          this.setState({ board: dupBoard, turn: "mother", spots: newSpots })
+          checkForDraw(newSpots)
         }
+      } else {
+        alert('this spot alr taken fam')
       }
     }
 
     render() {
         const turn = <div id="turn">{this.state.turn}</div>
+        const spots = <div id="spots" style={{display: 'invisible'}}>{this.state.spots}</div>
 
         console.log("board", this.state.board);
 
@@ -78,6 +94,7 @@ class Board extends React.Component {
           <div className="item">
             {board}
             {turn}
+            {spots}
           </div>
         );
     }
