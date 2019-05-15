684cc98c1d0bcf113d292040e997058c4d4d48b7
seantanjs <seantanjuisiang@gmail.com>
2019-05-15 08:40:29 +0800

    first commit, made the boxes

--- script.jsx
+++ script.jsx
@@ -1,8 +1,15 @@
+class Game extends React.Component {
+
+
+
+
+}
+
 class Board extends React.Component {
     constructor(){
 
       super()
-
+      this.flag = false;
       this.state = {
         board: [
           ['i','i','i'],
@@ -13,9 +20,15 @@ class Board extends React.Component {
 
     }
 
-    squareClick(something){
-        console.log( something );
+    squareClick(colIndex, rowIndex){
+        this.flag = !this.flag
+        if(this.flag) {
+
+
+        }
+        console.log(colIndex, rowIndex);
     }
+
 
     render() {
         console.log("board", this.state.board);
@@ -27,34 +40,35 @@ class Board extends React.Component {
 
             // make each column
             return (
-                    <p
-                        className="boo"
+                    <td>
+                    <button
+                        className="col"
                         key={colIndex}
                         onClick={()=>{
-                            this.squareClick(colIndex);
+                            this.squareClick(colIndex, rowIndex);
                         }}
-
                     >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
+                        <div>{colIndex} : {rowIndex}</div>
+                    </button>
+                    </td>
             );
 
           });
 
           // return the complete row
           return (
-            <div key={rowIndex} className="row">
+            <tr key={rowIndex} className="row">
               {rows}
-            </div>
+            </tr>
 
           );
 
         });
 
         return (
-          <div className="item">
+          <table className="item">
             {board}
-          </div>
+          </table>
         );
     }
 }
@@ -63,3 +77,35 @@ ReactDOM.render(
     <Board/>,
     document.getElementById('root')
 );
+
+// <table>
+//             <tr>
+//                 <td id="zero-zero"></td>
+//                 <td id="zero-one"></td>
+//                 <td id="zero-two"></td>
+//             </tr>
+//             <tr>
+//                 <td id="one-zero"></td>
+//                 <td id="one-one"></td>
+//                 <td id="one-two"></td>
+//             </tr>
+//             <tr>
+//                 <td id="two-zero"></td>
+//                 <td id="two-one"></td>
+//                 <td id="two-two"></td>
+//             </tr>
+//         </table>
+
+
+//         table {
+//     margin: 30px auto;
+//     border: 5px solid black;
+// }
+
+// td {
+//     border: 1px solid black;
+//     width: 150px;
+//     height: 150px;
+//     font-size: 80px;
+//     text-align: center;
+// }
