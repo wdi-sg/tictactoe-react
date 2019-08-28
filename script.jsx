ed15db96eb7c8e11dc70cc2383b0b639b768f605
shirleytwl <shirtanwl@gmail.com>
2019-08-28 17:05:22 +0800

    simple hard-coded game done

--- script.jsx
+++ script.jsx
@@ -1,61 +1,177 @@
 class Board extends React.Component {
     constructor(){
-
-      super()
-
-      this.state = {
-        board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
-        ]
-      }
+        super();
 
     }
+    state = {
+        board: [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+        ],
+        turn: 0,
+        currentPlayer: "✕",
+        winner: "",
+        loser: "",
+        gameEnd: false
+    };
 
-    squareClick(something){
-        console.log( something );
+    squareClick(row, col){
+        if (!this.state.gameEnd) {
+            if (this.state.board[row][col] === 0) {
+                if (this.state.turn % 2 === 0) {
+                    this.state.board[row][col] = 1;
+                    this.state.currentPlayer = "◯";
+                } else {
+                    this.state.board[row][col] = -1;
+                    this.state.currentPlayer = "✕";
+                }
+                this.setState({turn: this.state.turn + 1});
+
+                if (this.state.turn >= 2) {
+                    if (this.checkGame()) {
+                        this.setState({gameEnd: true});
+                    }
+                }
+            }
+        }
+    }
+
+    checkGame(){
+        let totalValue = 0;
+
+        //check horizontally
+        //loop through rows
+        for (let i=0;i<3;i++){
+            //loop through squares
+            for (let j=0;j<this.state.board[i].length;j++){
+                totalValue = totalValue + this.state.board[i][j];
+                if (totalValue === 3) {
+                    this.state.loser = "◯";
+                    this.state.winner = "✕";
+                    return true;
+                }
+                else if (totalValue === -3){
+                    this.state.loser = "✕";
+                    this.state.winner = "◯";
+                    return true;
+                }
+            }
+            totalValue = 0;
+        }
+
+        //check vertically
+        //loop through rows
+        let currentCol = 0;
+        for (let i=0;i<3;i++){
+            //loop through squares
+            for (let j=0;j<this.state.board[i].length;j++){
+                totalValue = totalValue + this.state.board[j][currentCol];
+                if (totalValue === 3) {
+                    this.state.loser = "◯";
+                    this.state.winner = "✕";
+                    return true;
+                }
+                else if (totalValue === -3){
+                    this.state.loser = "✕";
+                    this.state.winner = "◯";
+                    return true;
+                }
+            }
+            currentCol++;
+            totalValue = 0;
+        }
+
+        //check diagonally
+        for (let i=0;i<3;i++){
+            totalValue = totalValue + this.state.board[i][i];
+            //console.log(totalValue);
+            if (totalValue === 3) {
+                this.state.loser = "◯";
+                this.state.winner = "✕";
+                return true;
+            }
+            else if (totalValue === -3){
+                this.state.loser = "✕";
+                this.state.winner = "◯";
+                return true;
+            }
+        }
+        // check reverse diagonally
+        for (let i=0;i<3;i++){
+            totalValue = totalValue + this.state.board[i][3-(i+1)];
+            if (totalValue === 3) {
+                this.state.loser = "◯";
+                this.state.winner = "✕";
+                return true;
+            }
+            else if (totalValue === -3){
+                this.state.loser = "✕";
+                this.state.winner = "◯";
+                return true;
+            }
+        }
+        return false;
     }
 
     render() {
-        console.log("board", this.state.board);
 
         const board = this.state.board.map( (row,rowIndex) => {
-
-          // make a single row
-          const rows = row.map( (col,colIndex) => {
-
-            // make each column
-            return (
-                    <p
-                        className="boo"
+            const rows = row.map( (col,colIndex) => {
+                let display = "";
+                if (col>0) {
+                    display = "✕";
+                }
+                else if (col<0) {
+                    display = "◯";
+                }
+                return (
+                    <span
+                        className="col"
                         key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex);
+                        onClick={() => {
+                            this.squareClick(rowIndex, colIndex);
                         }}
-
                     >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
+                        {display}
+                    </span>
+                );
+            });
+            return (
+                <div key={rowIndex} className="row">
+                  {rows}
+                </div>
+
             );
 
-          });
-
-          // return the complete row
-          return (
-            <div key={rowIndex} className="row">
-              {rows}
-            </div>
-
-          );
-
         });
+        let message;
+
+        if (this.state.gameEnd) {
+            message = <p>Game ended! {this.state.winner} won!</p>
+        }
+        else {
+            message = <CurrentTurn player={this.state.currentPlayer}/>
+        }
 
         return (
-          <div className="item">
-            {board}
-          </div>
+            <div>
+                {message}
+                <div id="board">
+                    {board}
+                </div>
+            </div>
         );
+    }
+}
+
+class CurrentTurn extends React.Component {
+    render() {
+        return (
+            <div className="current-turn">
+                <p>{this.props.player}'s Turn</p>
+            </div>
+        )
     }
 }
 
