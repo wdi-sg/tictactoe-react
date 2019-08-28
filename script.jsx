c5d700083d06c93bcc0a08b181a9b0aaffb85e71
emijay <hilmi.johari@yahoo.com.sg>
2019-08-28 15:46:50 +0800

    able to alternate between X and O and not fill on a "filled" tile

--- script.jsx
+++ script.jsx
@@ -5,20 +5,43 @@ class Board extends React.Component {
 
       this.state = {
         board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
-        ]
+          ['','',''],
+          ['','',''],
+          ['','','']
+        ],
+        player: 1
       }
 
     }
 
-    squareClick(something){
-        console.log( something );
+    squareClick(columnIndex, rowIndex){
+
+        console.log(this.state.player)
+
+        let newBoard = this.state.board;
+        let currentPlayer = this.state.player;
+
+        if (newBoard[rowIndex][columnIndex] === "" && currentPlayer%2 === 1) {
+            newBoard[rowIndex][columnIndex] = "X"
+            currentPlayer = currentPlayer + 1
+        } else if (newBoard[rowIndex][columnIndex] === "" && currentPlayer%2 === 0){
+            newBoard[rowIndex][columnIndex] = "O"
+            currentPlayer = currentPlayer + 1
+        }
+
+        console.log(newBoard)
+
+        const newState = {
+            board : newBoard,
+            player : currentPlayer
+        }
+
+        this.setState(newState);
+
     }
 
     render() {
-        console.log("board", this.state.board);
+        // console.log("board", this.state.board);
 
         const board = this.state.board.map( (row,rowIndex) => {
 
@@ -27,28 +50,18 @@ class Board extends React.Component {
 
             // make each column
             return (
-                    <p
-                        className="boo"
-                        key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex);
-                        }}
-
-                    >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
+                <p className="boo" key={colIndex} onClick={()=>{ this.squareClick(colIndex, rowIndex);}}>
+                    {col}
+                </p>
             );
 
           });
-
           // return the complete row
           return (
             <div key={rowIndex} className="row">
               {rows}
             </div>
-
           );
-
         });
 
         return (
