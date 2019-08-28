08e0fa450c7c3b726d1ed774b1d59cdeb8f9dac9
kach92 <kenny.ang9206@gmail.com>
2019-08-28 15:16:01 +0800

    initial commit

--- script.jsx
+++ script.jsx
@@ -2,58 +2,186 @@ class Board extends React.Component {
     constructor(){
 
       super()
-
+      this.count = 0
       this.state = {
         board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
-        ]
+          ['','',''],
+          ['','',''],
+          ['','','']
+        ],
+        win: false,
+        winner:null
       }
 
     }
 
-    squareClick(something){
-        console.log( something );
+    checkWin(sym){
+        console.log("IN CHECKWIN")
+        let x = sym
+
+        let arr = this.state.board
+        var checkHorizontal = function(arr,sym){
+            var checkCounter = 0;
+            for(let i=0;i<arr.length;i++){
+                for(let j=0;j<arr.length;j++){
+                    if(arr[i][j]===sym){
+                         checkCounter++;
+                    }
+                    else{
+                        checkCounter = 0;
+                        break;
+                    }
+                }
+                if(checkCounter ===arr.length){
+                    break;
+                }
+                else{
+                    checkCounter = 0;
+                    continue;
+                }
+            }
+            if(checkCounter === arr.length)
+                return true
+            else
+                return false
+        }
+
+        //check vertical wins
+        var checkVertical = function(arr,sym){
+            var checkCounter = 0;
+            for(let i=0;i<arr.length;i++){
+                for(let j=0;j<arr.length;j++){
+                    if(arr[j][i]===sym){
+                         checkCounter++;
+                    }
+                    else{
+                        checkCounter = 0;
+                        break;
+                    }
+                }
+                if(checkCounter ===arr.length){
+                    break;
+                }
+                else{
+                    checkCounter = 0;
+                    continue;
+                }
+            }
+            if(checkCounter === arr.length)
+                return true
+            else
+                return false
+        }
+
+        //check X wins
+        var checkX = function(arr,sym){
+            var checkCounter = 0;
+            for(let i=0;i<arr.length;i++){
+                if(arr[i][i]===sym){
+                    checkCounter++;
+                }
+                else{
+                    checkCounter = 0;
+                    break;
+                }
+            }
+
+            if(checkCounter === arr.length){
+                return true
+            }else{
+                checkCounter =0;
+                var k = 0;
+                for(let j=arr.length-1;j>=0;j--){
+                    if(arr[k][j]===sym){
+                        checkCounter++;
+                        k++;
+                    }
+                    else{
+                        checkCounter = 0;
+                        break;
+                    }
+                }
+
+                if(checkCounter === arr.length){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+
+        }
+
+        let result = [checkHorizontal(arr,sym),checkVertical(arr,sym),checkX(arr,sym)]
+        if(result.includes(true)){
+            this.state.win = true
+            this.state.winner = sym
+        }
+
+        console.log(this.state.win)
+
+        let newState = {
+            win:this.state.win,
+            winner:this.state.winner
+        }
+
+        this.setState(newState)
+    }
+
+    squareClick(e,colIndex,rowIndex){
+        let sym = null
+        if(this.count%2 === 0){
+            this.state.board[colIndex][rowIndex] = "X"
+            sym = "X"
+        }else{
+            this.state.board[colIndex][rowIndex] = "O"
+            "O"
+        }
+        console.log(e.target)
+        let newState = {
+            board:this.state.board
+        }
+        this.setState(newState)
+        this.checkWin(sym)
+        this.count++
     }
 
     render() {
-        console.log("board", this.state.board);
 
         const board = this.state.board.map( (row,rowIndex) => {
 
           // make a single row
-          const rows = row.map( (col,colIndex) => {
+            const rows = row.map( (col,colIndex) => {
 
-            // make each column
-            return (
-                    <p
-                        className="boo"
-                        key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex);
-                        }}
+                // make each column
+                return (
+                        <p
+                            className="boo"
+                            key={colIndex}
+                            onClick={(e)=>{
+                                this.squareClick(e,colIndex,rowIndex);
+                            }}
 
-                    >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
-            );
+                        >
+                        {this.state.board[colIndex][rowIndex]}
+                        </p>
+                );
 
-          });
+            });
 
           // return the complete row
-          return (
-            <div key={rowIndex} className="row">
-              {rows}
-            </div>
+            return (
+                <div key={rowIndex} className="row">
+                    {rows}
+                </div>
 
-          );
+            );
 
         });
 
         return (
           <div className="item">
             {board}
+            {this.state.win && <p className="winning-msg">{this.state.winner} WINSSSSSS</p>}
           </div>
         );
     }
