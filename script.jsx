330150c1c5860d59860e677a030abe871b91917f
aqilahrajab <YOUR-EMAIL-ADDRESS>
2019-08-28 16:36:14 +0800

    Created tic-tac-toe board and enable the click function on each square

--- script.jsx
+++ script.jsx
@@ -1,64 +1,76 @@
-class Board extends React.Component {
-    constructor(){
 
-      super()
 
-      this.state = {
-        board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
-        ]
-      }
+//CREATE A CLASS FOR EACH SQUARE
+class Square extends React.Component {
 
+    constructor() {
+        super()
     }
 
-    squareClick(something){
-        console.log( something );
+    clickHandler() {
+        console.log('click!')
     }
 
+//create 1 button
     render() {
-        console.log("board", this.state.board);
-
-        const board = this.state.board.map( (row,rowIndex) => {
-
-          // make a single row
-          const rows = row.map( (col,colIndex) => {
-
-            // make each column
-            return (
-                    <p
-                        className="boo"
-                        key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex);
-                        }}
-
-                    >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
-            );
-
-          });
-
-          // return the complete row
-          return (
-            <div key={rowIndex} className="row">
-              {rows}
-            </div>
-
-          );
-
-        });
-
         return (
-          <div className="item">
-            {board}
-          </div>
+            <button className = "squareButtons" onClick={() => this.clickHandler}>
+                {this.props.value}
+            </button>
         );
     }
 }
 
+//CREATE A CLASS FOR ALL THE BOARDS >> which call the individual square above
+class Board extends React.Component {
+
+    //I want to render each square within each has its own index >> which is why renderSquare(i)
+    renderSquare(i) {
+        return <Square value={i}/>
+    }
+
+    //Multiply each square to the rows
+    render() {
+        return(
+
+            <div className = "whole-board">
+
+                <div className = "board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+
+                <div className = "board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+
+                <div className = "board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+
+            </div>
+        );
+    }
+}
+
+// //CREATE A CLASS FOR GAME >>which calls board- full layout of the game
+// class Game extends React.Components {
+
+//     render() {
+//         return(
+
+
+//         );
+//     }
+// }
+
+
+//RENDER MY BOARD TO THE INDEX.HTML PAGE
 ReactDOM.render(
     <Board/>,
     document.getElementById('root')
