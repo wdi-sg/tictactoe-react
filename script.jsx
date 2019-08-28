4e59b0a4583ac283d6546f7d8b4242b7cb42a9d1
kinskin <md.asshikin@gmail.com>
2019-08-28 19:01:30 +0800

    deliverable

--- script.jsx
+++ script.jsx
@@ -5,16 +5,87 @@ class Board extends React.Component {
 
       this.state = {
         board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
-        ]
+          ['☹','☹','☹'],
+          ['☹','☹','☹'],
+          ['☹','☹','☹']
+        ],
+        turn: 0,
+        counter1: 1
+
       }
 
     }
 
-    squareClick(something){
-        console.log( something );
+
+    checkingForWin(){
+        if(this.state.counter1 < 9){
+            if((this.state.board[0][0] === "✘" && this.state.board[0][1] === "✘" && this.state.board[0][2] === "✘" )|| (this.state.board[0][0] === "Ѻ" && this.state.board[0][1] === "Ѻ" && this.state.board[0][2] === "Ѻ")){
+                alert("Row one wins");
+
+            }
+            else if(this.state.board[1][0] === "✘" && this.state.board[1][1] === "✘" && this.state.board[1][2] === "✘" || this.state.board[1][0] === "Ѻ" && this.state.board[1][1] === "Ѻ" && this.state.board[1][2] === "Ѻ"){
+                alert("Row two wins");
+
+            }
+            else if(this.state.board[2][0] === "✘" && this.state.board[2][1] === "✘" && this.state.board[2][2] === "✘" || this.state.board[2][0] === "✘" && this.state.board[2][1] === "Ѻ" && this.state.board[2][2] === "Ѻ"){
+                alert("Row three wins");
+
+            }
+            else if(this.state.board[0][0] === "✘" && this.state.board[1][0] === "✘" && this.state.board[2][0] === "✘" || this.state.board[0][0] === "Ѻ" && this.state.board[1][0] === "Ѻ" && this.state.board[2][0] === "Ѻ"){
+                alert("column one wins");
+
+            }
+            else if(this.state.board[0][1] === "✘" && this.state.board[1][1] === "✘" && this.state.board[2][1] === "✘" || this.state.board[0][1] === "Ѻ" && this.state.board[1][1] === "Ѻ" && this.state.board[2][1] === "Ѻ"){
+                alert("column two wins");
+
+            }
+            else if(this.state.board[0][2] === "✘" && this.state.board[1][2] === "✘" && this.state.board[2][2] === "✘" || this.state.board[0][2] === "Ѻ" && this.state.board[1][2] === "Ѻ" && this.state.board[2][2] === "Ѻ"){
+                alert("column 3 wins");
+
+            }
+            else if(this.state.board[0][0] === "✘" && this.state.board[1][1] === "✘" && this.state.board[2][2] === "✘" || this.state.board[0][0] === "Ѻ" && this.state.board[1][1] === "Ѻ" && this.state.board[2][2] === "Ѻ"){
+                alert("ltr diagonal wins");
+
+            }
+            else if(this.state.board[0][2] === "✘" && this.state.board[1][1] === "✘" && this.state.board[2][0] === "✘" || this.state.board[0][2] === "Ѻ" && this.state.board[1][1] === "Ѻ" && this.state.board[2][0] === "Ѻ"){
+                alert("rtl diagonal wins")
+            }
+        }
+        else{
+            alert("ITS A DRAW");
+
+        }
+    }
+
+
+
+    squareClick(event, colIndex, rowIndex){
+        console.log( colIndex,rowIndex);
+        console.log(event.target.innerHTML);
+        let board = this.state.board;
+        let counter1 = this.state.counter1
+        if (event.target.innerHTML === '☹'){
+            if (counter1%2 === 0){
+                board[rowIndex][colIndex] = "✘"
+                counter1 += 1
+                const newBoard = {
+                    board: board,
+                    counter1: counter1
+                }
+                this.setState(newBoard)
+                this.checkingForWin();
+            }
+            else{
+                board[rowIndex][colIndex] = "Ѻ"
+                counter1 += 1
+                const newBoard = {
+                    board: board,
+                    counter1: counter1
+                }
+                this.setState(newBoard)
+                this.checkingForWin();
+            }
+        }
     }
 
     render() {
@@ -27,23 +98,16 @@ class Board extends React.Component {
 
             // make each column
             return (
-                    <p
-                        className="boo"
-                        key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex);
-                        }}
-
-                    >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
+                    <div className="boo btn-outline-danger" key={colIndex}  onClick={(event)=>{ this.squareClick(event, colIndex,rowIndex)}} >
+                        {this.state.board[rowIndex][colIndex]}
+                    </div>
             );
 
           });
 
           // return the complete row
           return (
-            <div key={rowIndex} className="row">
+            <div  className="row justify-content-center" key={rowIndex} >
               {rows}
             </div>
 
@@ -52,9 +116,14 @@ class Board extends React.Component {
         });
 
         return (
-          <div className="item">
-            {board}
-          </div>
+            <div className = 'container text-center'>
+                <div className = "card-header text-center">
+                    <h1>Turn: {this.state.counter1}</h1>
+                </div>
+                <div className="item mt-5">
+                {board}
+                </div>
+            </div>
         );
     }
 }
