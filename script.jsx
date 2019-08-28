82170f89ed9321ee93957bb6727dd3d7f83c0c39
Jonathan <jonathanbek93@gmail.com>
2019-08-29 01:42:35 +0800

    did diag checks, halfway through col check

--- script.jsx
+++ script.jsx
@@ -9,11 +9,11 @@ class Board extends React.Component {
           ['','',''],
           ['','','']
         ],
-        turn: 'X'
+        turn: 'X',
+        rows: 3,
       }
-
-
     }
+
     turnChange(){
         console.log('changing turn')
         let currentTurn = this.state.turn;
@@ -24,14 +24,67 @@ class Board extends React.Component {
             this.setState({turn: 'X'})
         };
     }
+
+    checkWinCondition(counterX, counterO){
+        let quota = this.state.rows;
+        if(counterX === 3){
+            alert('X wins!');
+        } else if(counterO === 3){
+            alert('Y wins!');
+        };
+    }
+
+    checkDiagFromTopLeft(){
+        let rows = this.state.rows;
+        let cols = this.state.rows;
+        let board = this.state.board;
+        let storage = [];
+        for(let i=0;i<rows;i++){
+            storage.push(board[i][i]);
+        };
+        let counterX = 0;
+        let counterO = 0;
+        for (let j=0;j<storage.length;j++){
+            if (storage[j] === 'X' ){
+                counterX += 1;
+            } else if (storage[j] === 'O'){
+                counterO += 1;
+            };
+        };
+        this.checkWinCondition(counterX, counterO)
+    }
+
+    checkDiagFromBotLeft(){
+        let rows = this.state.rows;
+        let cols = this.state.rows;
+        let board = this.state.board;
+        let storage = [];
+        for(let i=0;i<rows;i++){
+            storage.push(board[rows-i-1][i]);
+        };
+        let counterX = 0;
+        let counterO = 0;
+        for (let j=0;j<storage.length;j++){
+            if (storage[j] === 'X' ){
+                counterX += 1;
+            } else if (storage[j] === 'O'){
+                counterO += 1;
+            };
+        };
+        this.checkWinCondition(counterX, counterO)
+    }
+
     squareClick(rowIndex, colIndex){
         let currentBoard = this.state.board;
         currentBoard[rowIndex][colIndex] = this.state.turn;
         console.log(currentBoard);
-        this.setState({board: currentBoard})
-        this.turnChange()
-
+        this.setState({board: currentBoard});
+        this.turnChange();
+        this.checkDiagFromTopLeft();
+        this.checkDiagFromBotLeft();
     }
+
+
 
     render() {
         console.log("board", this.state.board);
