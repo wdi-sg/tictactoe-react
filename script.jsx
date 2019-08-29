e92c45a5fe631bfbdbea16edf2caa4dc97f07dcb
Teo Boon Hock <hockie2@yahoo.com.sg>
2019-08-29 08:46:44 +0800

    button clickable w X and O

--- script.jsx
+++ script.jsx
@@ -1,24 +1,69 @@
+
+
+
 class Board extends React.Component {
     constructor(){
-
-      super()
-
+      super();
       this.state = {
         board: [
           ['i','i','i'],
           ['i','i','i'],
           ['i','i','i']
-        ]
-      }
-
+        ],
+        turn : 0,
+        player: null
+      };
     }
 
-    squareClick(something){
-        console.log( something );
+   // calculateWinner(squares){
+   //      const lines = [
+   //                  [0,1,2],
+   //                  [3,4,5],
+   //                  [6,7,8],
+   //                  [0,3,6],
+   //                  [1,4,7],
+   //                  [2,5,8],
+   //                  [0,4,8],
+   //                  [2,4,6]
+   //                  ];
+
+   //      for (let i = 0; i < lines.length; i++) {
+   //          const[a,b,c] = lines[i];
+   //          if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
+   //              return squares[a]
+   //          }
+   //      }
+   //      return null;
+   //  }
+
+
+    squareClick(event){
+        // console.log( event );
+        // const winner = this.calculateWinner(squares);
+        // if (winner || squares[event]){
+        //     return;
+        // }
+
+        if (event.target.textContent === "") {
+            if (this.state.turn % 2 === 0) {
+                this.state.player = "X";
+                event.target.textContent = this.state.player;
+                this.state.turn++;
+            } else {
+                this.state.player = "O";
+                event.target.textContent = this.state.player;
+                this.state.turn++;
+            }
+
+            let newState = {
+                board:this.state.board
+            }
+            this.setState(newState)
+        }
     }
 
     render() {
-        console.log("board", this.state.board);
+        // console.log("board", this.state.board);
 
         const board = this.state.board.map( (row,rowIndex) => {
 
@@ -27,16 +72,13 @@ class Board extends React.Component {
 
             // make each column
             return (
-                    <p
+                    <button type="checkbox"
                         className="boo"
                         key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex);
-                        }}
-
+                        onClick={(event)=>{this.squareClick(event);}}
                     >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
+                        {/*{col} : {colIndex} : {rowIndex}*/}
+                    </button>
             );
 
           });
