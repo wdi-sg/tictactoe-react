f7941a7df277f154d15fd3840ca47c86b445a41e
Nathanael Tan <nathanael.tan92@gmail.com>
2019-11-22 08:36:40 +0800

    started on game logic but some bugs that still havent fixed yet, the top right and top middle boxes auto log wins

--- src/client/components/board/board.jsx
+++ src/client/components/board/board.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styles from './style.scss'
 import { hot } from 'react-hot-loader';
+
+
+  
 class Board extends React.Component {
     constructor(){
 
@@ -8,22 +11,51 @@ class Board extends React.Component {
 
       this.state = {
         board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
+          [null,null,null],
+          [null,null,null],
+          [null,null,null]
         ],
-        player: false
+        player: false,
+        win: false,
+        winner: null
+
       }
 
     }
+
+  
 
     squareClick(i, j){
       console.log(this.state.player)
       let changeBoard = this.state.board;
       changeBoard[i][j] = this.state.player ? "X" : "O";
-      
+      console.log(changeBoard)
       console.log(i, j)
-        this.setState({board: changeBoard, player: !this.state.player})
+      this.setState({board: changeBoard, player: !this.state.player})
+
+      
+      for(let i = 0; i < changeBoard.length; i++) {
+       if(changeBoard[i][0] !== null || changeBoard[i][1] !==null || changeBoard[i][2] !== null ) {
+
+        if(changeBoard[i][0] === changeBoard[i][1] && changeBoard[i][0] === changeBoard[i][2]) {
+          alert("ROW")
+        } 
+        if(changeBoard[i][i] === changeBoard[i + 1][i + 1] && changeBoard[i][i] === changeBoard[i + 2][i + 2]) {
+          alert("DIAGONAL")
+        }
+        for (let j = 0; j < 3; j++) {
+          // Checking the Columns for a Win
+          if(changeBoard[0][j] !== null || changeBoard[1][j] !==null || changeBoard[2][j] !== null  ) {
+            if (changeBoard[i][j] ===changeBoard[i + 1][j] &&changeBoard[i][j] ===changeBoard[i + 2][j]) {
+              alert("COLUMN")
+          }
+          }
+    
+        }
+       }
+          
+        
+      }
     }
 
     render() {
@@ -44,7 +76,7 @@ class Board extends React.Component {
                         }}
 
                     >
-                     {col} 
+                     <p>{col} </p>
                     </div>
             );
 
@@ -69,3 +101,6 @@ class Board extends React.Component {
 }
 
 export default Board;
+
+
+
