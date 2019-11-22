6c34d1398c3c2f46c923737bed656cd93e3fa510
Sharon <sharxnrxse1312@gmail.com>
2019-11-22 10:15:26 +0800

    Able to display X and O

--- src/client/components/board/board.jsx
+++ src/client/components/board/board.jsx
@@ -1,64 +1,106 @@
 import React from 'react';
+import styles from './style.scss';
+
 
 class Board extends React.Component {
-    constructor(){
+  constructor() {
 
-      super()
+    super()
 
-      this.state = {
-        board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
-        ]
-      }
-
+    this.state = {
+      board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+      ],
     }
+    this.turn = 0;
+    this.stage = 0;
+    // win = false;
+  }
 
-    squareClick(something, something2){
-        console.log( something, something2 );
+  checkWin() {
+    let board = this.state.board.toString();
+    console.log('REGEX', board);
+    if (/XXX|X...X...X|X....X....X|X..X..X/.test(board)) {
+      alert("X WLLELEELE");
+    } else if (/OOO|O...O...O|O....O....O| O..O..O /.test(board)) {
+      alert("0 WLLELEELE");
+    } else {
+      return false;
     }
+  };
 
-    render() {
-        console.log("board", this.state.board);
 
-        const board = this.state.board.map( (row,rowIndex) => {
 
-          // make a single row
-          const rows = row.map( (col,colIndex) => {
+  squareClick(colIndex, rowIndex) {
+    console.log("Selected box:", colIndex, rowIndex);
+    if (this.turn % 2 === 0 && this.stage < 9) {
+      this.state.board[colIndex][rowIndex] = "X"
+      this.checkWin();
+      // let currentState = {
+      //   board: this.state.board
+      // }
+      // this.setState(currentState);
+    } else if (this.turn % 2 !== 0 && this.stage < 9) {
+      this.state.board[colIndex][rowIndex] = "O"
+      this.checkWin();
+    }
+    this.turn += 1;
+    this.stage += 1;
+    // console.log("HAAHH", this.state.board);
+    // console.log("length", this.state.board.length)
+    let currentState = {
+      board: this.state.board
+    }
+    this.setState(currentState);
 
-            // make each column
-            return (
-                    <p
-                        className="boo"
-                        key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex, rowIndex);
-                        }}
 
-                    >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
-            );
+  }
 
-          });
 
-          // return the complete row
-          return (
-            <div key={rowIndex} className="row">
-              {rows}
-            </div>
 
-          );
+  render() {
+    // save board status into a variable
+    console.log("THIS IS YOUR BOARD STATUS", this.state.board);
 
-        });
+    const board = this.state.board.map((row, rowIndex) => {
+      // const boardStatus = this.state.board;
+      // make a single row
+      const rows = row.map((col, colIndex) => {
 
+        // make each column
         return (
-          <div className="item">
-            {board}
-          </div>
+          <p
+            className="boo"
+            key={colIndex}
+            onClick={() => {
+              this.squareClick(colIndex, rowIndex);
+            }}
+
+          >
+            {this.state.board[colIndex][rowIndex]}
+          </p>
         );
-    }
+
+      });
+
+      // return the complete row
+      return (
+        <div key={rowIndex} className={styles.board}>
+          {rows}
+        </div>
+
+      );
+
+    });
+
+    return (
+      <div className="item">
+        {board}
+      </div>
+    );
+  }
 }
 
 export default Board;
