f0f75f11293f7d4bf7f00bd2389fa4e3954f03f1
dannyozh <danielongzhihao@gmail.com>
2019-11-22 08:59:53 +0800

    tried regex, has some bugs...

--- src/client/components/board/board.jsx
+++ src/client/components/board/board.jsx
@@ -16,23 +16,49 @@ class Board extends React.Component {
     }
     this.turn = 0;
     this.stage = 0;
+    // win = false;
   }
+
+  checkWin() {
+    let board = this.state.board.toString();
+    console.log('REGEX', board);
+    if (/XXX|X...X...X|X....X....X|X..X..X/.test(board)) {
+      alert("X WLLELEELE");
+    } else if (/OOO|O...O...O|O....O....O| O..O..O /.test(board)) {
+      alert("0 WLLELEELE");
+    } else {
+      return false;
+    }
+  };
+
+
 
   squareClick(colIndex, rowIndex) {
     console.log("Selected box:", colIndex, rowIndex);
     if (this.turn % 2 === 0 && this.stage < 9) {
       this.state.board[colIndex][rowIndex] = "X"
+      this.checkWin();
+      // let currentState = {
+      //   board: this.state.board
+      // }
+      // this.setState(currentState);
     } else if (this.turn % 2 !== 0 && this.stage < 9) {
       this.state.board[colIndex][rowIndex] = "O"
+      this.checkWin();
     }
     this.turn += 1;
     this.stage += 1;
-    console.log(this.state.board);
+    // console.log("HAAHH", this.state.board);
+    // console.log("length", this.state.board.length)
     let currentState = {
       board: this.state.board
     }
     this.setState(currentState);
+
+
   }
+
+
 
   render() {
     // save board status into a variable
