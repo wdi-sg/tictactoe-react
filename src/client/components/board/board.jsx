8810da88afbcb637c33098c7a501a63d7118df23
wickker <choydianchun@gmail.com>
2020-05-29 18:01:57 +0800

    able to display X and O accurately upon click; unable to change option after clicking once

--- src/client/components/board/board.jsx
+++ src/client/components/board/board.jsx
@@ -17,25 +17,45 @@ class Board extends React.Component {
 
   squareClick(rowIndex, colIndex) {
     console.log(rowIndex, colIndex);
-    
+    let prevMoves = this.state.prevMoves;
+    let currentBoard= this.state.board;
+
+    if (prevMoves.length === 0) {
+      currentBoard[rowIndex][colIndex] = "X";
+      prevMoves.push("X");
+    } else if (prevMoves[prevMoves.length - 1] === "X" && currentBoard[rowIndex][colIndex] === "") {
+      currentBoard[rowIndex][colIndex] = "O";
+      prevMoves.push("O");
+    } else if (prevMoves[prevMoves.length - 1] === "O" && currentBoard[rowIndex][colIndex] === "") {
+      currentBoard[rowIndex][colIndex] = "X";
+      prevMoves.push("X");
+    } 
+
+    this.setState({ board: currentBoard, prevMoves: prevMoves });
   }
 
   render() {
-    console.log("board", this.state.board);
+    console.log("board: ", this.state.board);
+    console.log("previous moves: ", this.state.prevMoves);
 
     const board = this.state.board.map((row, rowIndex) => {
       // make a single row
       const rows = row.map((col, colIndex) => {
         // make each column
+        // let isDisabled = false;
+        // if (this.state.board[rowIndex][colIndex] !== "") {
+        //   isDisabled = true;
+        // }
         return (
           <div
             className={styles.boo}
             key={colIndex}
+            // disabled={isDisabled}
             onClick={() => {
               this.squareClick(rowIndex, colIndex);
             }}
           >
-            {col} : {rowIndex} : {colIndex}
+            {col}
           </div>
         );
       });
@@ -48,7 +68,9 @@ class Board extends React.Component {
       );
     });
 
-    return <div className="item">{board}</div>;
+    return (
+      <div className="item">{board}</div>
+    );
   }
 }
 
