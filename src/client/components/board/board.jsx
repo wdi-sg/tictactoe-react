998993090a1482f7d52919e298c32ecc93bbd1c5
wickker <choydianchun@gmail.com>
2020-05-29 17:33:49 +0800

    formatted grid

--- src/client/components/board/board.jsx
+++ src/client/components/board/board.jsx
@@ -1,64 +1,55 @@
-import React from 'react';
+import React from "react";
+import styles from "./board.scss";
 
 class Board extends React.Component {
-    constructor(){
+  constructor() {
+    super();
 
-      super()
+    this.state = {
+      board: [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
+      ],
+      prevMoves: []
+    };
+  }
 
-      this.state = {
-        board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
-        ]
-      }
+  squareClick(rowIndex, colIndex) {
+    console.log(rowIndex, colIndex);
+    
+  }
 
-    }
+  render() {
+    console.log("board", this.state.board);
 
-    squareClick(something, something2){
-        console.log( something, something2 );
-    }
-
-    render() {
-        console.log("board", this.state.board);
-
-        const board = this.state.board.map( (row,rowIndex) => {
-
-          // make a single row
-          const rows = row.map( (col,colIndex) => {
-
-            // make each column
-            return (
-                    <p
-                        className="boo"
-                        key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex, rowIndex);
-                        }}
-
-                    >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
-            );
-
-          });
-
-          // return the complete row
-          return (
-            <div key={rowIndex} className="row">
-              {rows}
-            </div>
-
-          );
-
-        });
-
+    const board = this.state.board.map((row, rowIndex) => {
+      // make a single row
+      const rows = row.map((col, colIndex) => {
+        // make each column
         return (
-          <div className="item">
-            {board}
+          <div
+            className={styles.boo}
+            key={colIndex}
+            onClick={() => {
+              this.squareClick(rowIndex, colIndex);
+            }}
+          >
+            {col} : {rowIndex} : {colIndex}
           </div>
         );
-    }
+      });
+
+      // return the complete row
+      return (
+        <div key={rowIndex} className={styles.row}>
+          {rows}
+        </div>
+      );
+    });
+
+    return <div className="item">{board}</div>;
+  }
 }
 
 export default Board;
