e0747d383d2240ad992f357ba71a1b43380fabca
kenneththesheep <kenneththesheep@gmail.com>
2020-05-29 16:59:16 +0800

    did the minimal mvp

--- src/client/components/board/board.jsx
+++ src/client/components/board/board.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+let turns = 0;
+
 class Board extends React.Component {
     constructor(){
 
@@ -17,6 +19,22 @@ class Board extends React.Component {
 
     squareClick(something, something2){
         console.log( something, something2 );
+        var playBoard = this.state.board;
+        console.log(playBoard)
+        console.log(playBoard[something][something2])
+        if(turns%2==0)
+            {
+                playBoard[something2][something]= "X"
+            }
+        if(turns%2==1){
+                playBoard[something2][something]= "O"
+            }
+            turns++;
+        //console.log("clicking", currentValue);
+        // set the state of this component
+        console.log(this.state.board[something][something2]);
+        //this.setState( { board[something][something2]: currentValue } );
+        this.setState( { board: playBoard } );
     }
 
     render() {
@@ -29,23 +47,26 @@ class Board extends React.Component {
 
             // make each column
             return (
-                    <p
-                        className="boo"
+                    <div
+                        //className="boo"
+                        className="col-4 text-center pt-4 pb-4 my-auto border"
                         key={colIndex}
+
                         onClick={()=>{
                             this.squareClick(colIndex, rowIndex);
                         }}
 
                     >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
+                        {col}
+
+                    </div>
             );
 
           });
 
           // return the complete row
           return (
-            <div key={rowIndex} className="row">
+            <div key={rowIndex} className="row" style = {{height:"100px"}}>
               {rows}
             </div>
 
