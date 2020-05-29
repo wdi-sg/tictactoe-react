4aeb34e52e50e5da612419d31deac62eeecaa505
wickker <choydianchun@gmail.com>
2020-05-29 18:14:30 +0800

    restart game function works

--- src/client/components/board/board.jsx
+++ src/client/components/board/board.jsx
@@ -11,26 +11,42 @@ class Board extends React.Component {
         ["", "", ""],
         ["", "", ""],
       ],
-      prevMoves: []
+      prevMoves: [],
     };
   }
 
   squareClick(rowIndex, colIndex) {
     console.log(rowIndex, colIndex);
     let prevMoves = this.state.prevMoves;
-    let currentBoard= this.state.board;
+    let currentBoard = this.state.board;
 
     if (prevMoves.length === 0) {
       currentBoard[rowIndex][colIndex] = "X";
       prevMoves.push("X");
-    } else if (prevMoves[prevMoves.length - 1] === "X" && currentBoard[rowIndex][colIndex] === "") {
+    } else if (
+      prevMoves[prevMoves.length - 1] === "X" &&
+      currentBoard[rowIndex][colIndex] === ""
+    ) {
       currentBoard[rowIndex][colIndex] = "O";
       prevMoves.push("O");
-    } else if (prevMoves[prevMoves.length - 1] === "O" && currentBoard[rowIndex][colIndex] === "") {
+    } else if (
+      prevMoves[prevMoves.length - 1] === "O" &&
+      currentBoard[rowIndex][colIndex] === ""
+    ) {
       currentBoard[rowIndex][colIndex] = "X";
       prevMoves.push("X");
-    } 
+    }
 
+    this.setState({ board: currentBoard, prevMoves: prevMoves });
+  }
+
+  restartGame() {
+    let prevMoves = [];
+    let currentBoard = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
     this.setState({ board: currentBoard, prevMoves: prevMoves });
   }
 
@@ -42,15 +58,10 @@ class Board extends React.Component {
       // make a single row
       const rows = row.map((col, colIndex) => {
         // make each column
-        // let isDisabled = false;
-        // if (this.state.board[rowIndex][colIndex] !== "") {
-        //   isDisabled = true;
-        // }
         return (
           <div
             className={styles.boo}
             key={colIndex}
-            // disabled={isDisabled}
             onClick={() => {
               this.squareClick(rowIndex, colIndex);
             }}
@@ -69,7 +80,19 @@ class Board extends React.Component {
     });
 
     return (
-      <div className="item">{board}</div>
+      <div>
+        <div className="item">{board}</div>
+        <div>
+          <button
+            className={styles.clicky}
+            onClick={() => {
+              this.restartGame();
+            }}
+          >
+            Restart Game
+          </button>
+        </div>
+      </div>
     );
   }
 }
