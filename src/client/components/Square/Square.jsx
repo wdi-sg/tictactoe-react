27ffe2065bef265affebc76dec14b298ebb85915
Zachariah Chow <zachariahchow@gmail.com>
2020-05-30 23:18:05 +0800

    feat: Add Square functional component

---
+++ src/client/components/Square/Square.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Square = ({ value, onClick }) => {
+    const squareClass = value ? `squares ${value}` : `squares`;
+
+    return (
+        <button className={squareClass} onClick={onClick}>
+            {value}
+        </button>
+    )
+}
