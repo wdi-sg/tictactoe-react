6c4d4cc24d4796c2a04d92ce4966f7af962035e7
ThomasOh92 <ohcsthomas@gmail.com>
2020-05-30 16:49:11 +0800

    Can play game, can detect winning states

--- src/client/components/board/board.jsx
+++ src/client/components/board/board.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import Winmessage from './winmessage.jsx'
 
 class Board extends React.Component {
+  //Constructor for initial state - Set initial player and blank board
     constructor(){
 
       super()
@@ -10,15 +12,83 @@ class Board extends React.Component {
           ['i','i','i'],
           ['i','i','i'],
           ['i','i','i']
-        ]
+        ],
+        player: "X",
+        winmessage: ""
       }
 
     }
+    //Win condition
+    someoneHasWon(){
+      let matchCounterDiagonal = 0;
+      let matchCounterReverseDiagonal = 0;
+      let boardSize = 3;
+      let winLength = 3;
 
-    squareClick(something, something2){
-        console.log( something, something2 );
+      for (let i = 0; i < boardSize; i++){
+        let matchCounterRow = 0;
+        let matchCounterColumn = 0;
+        //Diagonals
+        if (this.state.board[i][i] === this.state.player){
+            matchCounterDiagonal++
+        }
+        if (this.state.board[boardSize - i - 1][i] === this.state.player){
+            matchCounterReverseDiagonal++
+        }
+        //Rows and Columns
+        for (let k = 0; k < boardSize; k++){
+            if (this.state.board[i][k] === this.state.player){
+                matchCounterRow++
+            }
+            if (this.state.board[k][i] === this.state.player){
+                matchCounterColumn++
+            }
+        }
+        //Check for matches
+        if (matchCounterRow === winLength || matchCounterColumn === winLength ||matchCounterDiagonal === winLength ||matchCounterReverseDiagonal === winLength){
+            return true
+        }
     }
 
+    }
+
+    //Click Handler 
+    squareClick(event, colIndex, rowIndex){
+        console.log( colIndex, rowIndex );
+        console.log(event.target);
+        let updatedObj = this.state
+        //Update board
+        updatedObj.board[rowIndex][colIndex] = updatedObj.player
+        //Check Win
+        if (this.someoneHasWon()){
+          console.log("win!")
+          updatedObj.winmessage = "You win young fella"
+        }
+        //Player switch
+        if (updatedObj.player === "X"){
+          updatedObj.player = "O"
+        } else {
+          updatedObj.player = "X"
+        }
+        //Update state and re-render board
+        this.setState(updatedObj)        
+    }
+
+    //Reset Game Click Handler
+    resetClick(){
+      let resetObj = {
+        board: [
+          ['i','i','i'],
+          ['i','i','i'],
+          ['i','i','i']
+        ],
+        player: "X",
+        winmessage: ""
+      }
+      this.setState(resetObj)
+    }
+
+  //Render Board
     render() {
         console.log("board", this.state.board);
 
@@ -29,16 +99,17 @@ class Board extends React.Component {
 
             // make each column
             return (
-                    <p
+                    <button
+                        type="checkbox"
                         className="boo"
                         key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex, rowIndex);
+                        onClick={(ev)=>{
+                            this.squareClick(ev, colIndex, rowIndex);
                         }}
 
                     >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
+                        {col} - {colIndex} : {rowIndex}
+                    </button>
             );
 
           });
@@ -56,6 +127,8 @@ class Board extends React.Component {
         return (
           <div className="item">
             {board}
+            <Winmessage message = {this.state.winmessage}/>
+            <button onClick={()=>{this.resetClick()}}>Reset Game</button>
           </div>
         );
     }
