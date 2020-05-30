5da0b33cb5b00ac84ce615235032857253557624
Zachariah Chow <zachariahchow@gmail.com>
2020-05-30 23:19:18 +0800

    feat: Update Board to functional component

--- src/client/components/board/board.jsx
+++ src/client/components/board/board.jsx
@@ -1,64 +1,12 @@
 import React from 'react';
+import Square from '../Square/Square';
 
-class Board extends React.Component {
-    constructor(){
-
-      super()
-
-      this.state = {
-        board: [
-          ['i','i','i'],
-          ['i','i','i'],
-          ['i','i','i']
-        ]
-      }
-
-    }
-
-    squareClick(something, something2){
-        console.log( something, something2 );
-    }
-
-    render() {
-        console.log("board", this.state.board);
-
-        const board = this.state.board.map( (row,rowIndex) => {
-
-          // make a single row
-          const rows = row.map( (col,colIndex) => {
-
-            // make each column
-            return (
-                    <p
-                        className="boo"
-                        key={colIndex}
-                        onClick={()=>{
-                            this.squareClick(colIndex, rowIndex);
-                        }}
-
-                    >
-                        {col} : {colIndex} : {rowIndex}
-                    </p>
-            );
-
-          });
-
-          // return the complete row
-          return (
-            <div key={rowIndex} className="row">
-              {rows}
-            </div>
-
-          );
-
-        });
-
-        return (
-          <div className="item">
-            {board}
-          </div>
-        );
-    }
-}
+const Board = ({ squares, onClick }) => (
+    <div className="board">
+        {squares.map((square, index) => (
+            <Square key={index} value={square} onClick={() => onClick(index)}/>
+        ))}
+    </div>
+);
 
 export default Board;
