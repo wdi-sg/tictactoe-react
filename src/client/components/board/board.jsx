799e9fd61c156112dd08d062cefa79ab28074700
YusofGotboudine <yusof95@hotmail.com>
2020-06-01 07:53:34 +0800

    added player tracking and turn counter

--- src/client/components/board/board.jsx
+++ src/client/components/board/board.jsx
@@ -10,18 +10,57 @@ class Board extends React.Component {
         ['', '', ''],
         ['', '', ''],
         ['', '', '']
-      ]
+      ],
+      turns: 0,
+      cursor: 'X',
+      maxTurns: 8,
+      playerTurn: 0
+
     }
 
   }
 
   squareClick(colIndex, rowIndex) {
     console.log(colIndex, rowIndex);
-    this.setState('X')
+  }
+
+  playerClick() {
+    console.log('Player ' + this.state.cursor + "'s Move");
+    console.log("clicked");
+
+    if (this.state.turns <= this.state.maxTurns) {
+      var turns = this.state.turns + 1;
+      this.setState({ turns: turns });
+
+      if (this.state.cursor == 'X') {
+        this.setState({ cursor: 'O' });
+      }
+      else if (this.state.cursor == 'O') {
+        this.setState({ cursor: 'X' });
+      }
+    }//alternating players
+
+    console.log("moves made :", this.state.turns + 1);
   }
 
   render() {
+    console.log("rendering");
     console.log("board", this.state.board);
+
+    // while (this.state.turns <= this.state.maxTurns) {
+    //   var playerTurn = this.state.playerTurn;
+    //   if (this.state.playerTurn == 0) {
+    //     this.setState({ cursor: 'X' });
+    //     playerTurn++;
+    //     this.setState({ playerTurn: playerTurn });
+    //   }
+    //   else if (playerTurn == 1) {
+    //     this.setState({ cursor: 'O' });
+    //     playerTurn--;
+    //     this.setState({ playerTurn: playerTurn });
+    //   }
+
+    // }//end of while-loop
 
     const board = this.state.board.map((row, rowIndex) => {
 
@@ -35,6 +74,7 @@ class Board extends React.Component {
             key={colIndex}
             onClick={() => {
               this.squareClick(colIndex, rowIndex);
+              this.playerClick();
             }}
 
           >
@@ -52,16 +92,6 @@ class Board extends React.Component {
       );
 
     });
-
-    const maxTurns = 8;
-    let playerTurn = 0;
-    let cursor;
-
-    if (playerTurn == 0) {
-      cursor = 'O';
-
-    }
-
 
     return (
       <div className="item">
